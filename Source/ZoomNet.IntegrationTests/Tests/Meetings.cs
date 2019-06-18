4a32243e3900bc704c6eb6f97111765c25115aa5
Jeremie Desautels <desautelsj@hotmail.com>
2019-06-18 11:13:29 -0400

    Cleanup during integration tests

--- Source/ZoomNet.IntegrationTests/Tests/Meetings.cs
+++ Source/ZoomNet.IntegrationTests/Tests/Meetings.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ZoomNet.Models;
@@ -26,15 +27,16 @@ namespace ZoomNet.IntegrationTests.Tests
 			await log.WriteLineAsync($"There are {paginatedUpcomingMeetings.TotalRecords} upcoming meetings").ConfigureAwait(false);
 
 			// CLEANUP PREVIOUS INTEGRATION TESTS THAT MIGHT HAVE BEEN INTERRUPTED BEFORE THEY HAD TIME TO CLEANUP AFTER THEMSELVES
-			//var cleanUpTasks = paginatedScheduledMeetings.Records
-			//    .Where(m => m.Name.StartsWith("ZoomNet Integration Testing:"))
-			//    .Select(async oldMeeting =>
-			//    {
-			//        await client.Meetings.DeleteAsync(userId, oldAccount.Id, null, cancellationToken).ConfigureAwait(false);
-			//        await log.WriteLineAsync($"Meeting {oldMeeting.Id} deleted").ConfigureAwait(false);
-			//        await Task.Delay(250).ConfigureAwait(false);    // Brief pause to ensure Zoom has time to catch up
-			//    });
-			//await Task.WhenAll(cleanUpTasks).ConfigureAwait(false);
+			var cleanUpTasks = paginatedScheduledMeetings.Records
+				.Union(paginatedLiveMeetings.Records)
+				.Where(m => m.Topic.StartsWith("ZoomNet Integration Testing:"))
+				.Select(async oldMeeting =>
+				{
+					await client.Meetings.DeleteAsync(userId, oldMeeting.Id, null, cancellationToken).ConfigureAwait(false);
+					await log.WriteLineAsync($"Meeting {oldMeeting.Id} deleted").ConfigureAwait(false);
+					await Task.Delay(250).ConfigureAwait(false);    // Brief pause to ensure Zoom has time to catch up
+				});
+			await Task.WhenAll(cleanUpTasks).ConfigureAwait(false);
 
 			var settings = new MeetingSettings()
 			{
@@ -61,6 +63,10 @@ namespace ZoomNet.IntegrationTests.Tests
 			};
 			var newRecurringMeeting = await client.Meetings.CreateRecurringMeetingAsync(userId, "ZoomNet Integration Testing: recurring meeting", "The agenda", start, duration, recurrenceInfo, "p@ss!w0rd", settings, trackingFields, cancellationToken).ConfigureAwait(false);
 			await log.WriteLineAsync($"Recurring meeting {newRecurringMeeting.Id} created").ConfigureAwait(false);
+
+			await client.Meetings.DeleteAsync(userId, newInstantMeeting.Id, null, cancellationToken).ConfigureAwait(false);
+			await client.Meetings.DeleteAsync(userId, newScheduledMeeting.Id, null, cancellationToken).ConfigureAwait(false);
+			await client.Meetings.DeleteAsync(userId, newRecurringMeeting.Id, null, cancellationToken).ConfigureAwait(false);
 		}
 	}
 }
