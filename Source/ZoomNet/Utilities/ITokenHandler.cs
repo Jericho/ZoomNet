8b6e8cabe3e0d8d806ef586db66c0445045c554b
Jericho <desautelsj@hotmail.com>
2020-05-25 11:02:43 -0400

    Add XML comments

--- Source/ZoomNet/Utilities/ITokenHandler.cs
+++ Source/ZoomNet/Utilities/ITokenHandler.cs
@@ -1,7 +1,15 @@
 namespace ZoomNet.Utilities
 {
+	/// <summary>
+	/// Interface for token handlers.
+	/// </summary>
 	internal interface ITokenHandler
 	{
+		/// <summary>
+		/// Refresh the access token if the previous one has expired.
+		/// </summary>
+		/// <param name="forceRefresh">Indicates if the token should be refreshes even if it's not expired.</param>
+		/// <returns>The refreshed access token or the current token if we determined that it didn't need to be refreshed.</returns>
 		string RefreshTokenIfNecessary(bool forceRefresh);
 	}
 }
