fa3a069d71141d4e2644ea370bcb61e0d66094f3
Jericho <desautelsj@hotmail.com>
2020-09-07 09:24:30 -0400

    Integration tests for Dashboards

---
+++ Source/ZoomNet.IntegrationTests/Tests/Dashboards.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using ZoomNet.Models;
+
+namespace ZoomNet.IntegrationTests.Tests
+{
+	public class Dashboards : IIntegrationTest
+	{
+		public async Task RunAsync(string userId, IZoomClient client, TextWriter log, CancellationToken cancellationToken)
+		{
+			if (cancellationToken.IsCancellationRequested) return;
+
+			await log.WriteLineAsync("\n***** DASHBOARDS *****\n").ConfigureAwait(false);
+
+			// GET ALL THE MEETINGS
+			var pastMeetingsStats = await client.Dashboards.GetAllMeetingsAsync(DateTime.UtcNow.AddMonths(-1), DateTime.UtcNow, DashboardMeetingType.Past, 100, null, cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"There are {pastMeetingsStats.TotalRecords} meetings in the last month").ConfigureAwait(false);
+		}
+	}
+}
