af7164cfca5beec3c504f901b884e0fe9ef07ad7
Jericho <desautelsj@hotmail.com>
2021-02-13 10:24:00 -0500

    (GH-99) Change the type of the Recording.Id property back to long

--- Source/ZoomNet/Models/Recording.cs
+++ Source/ZoomNet/Models/Recording.cs
@@ -16,7 +16,7 @@ namespace ZoomNet.Models
 		/// <summary>Gets or sets the recording id.</summary>
 		/// <value>The id.</value>
 		[JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
-		public string Id { get; set; }
+		public long Id { get; set; }
 
 		/// <summary>Gets or sets the ID of the user account.</summary>
 		/// <value>The account id.</value>
