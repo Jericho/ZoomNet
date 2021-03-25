f44d0467c2167ae99d2e94fdcc121e1b38d89288
Jericho <desautelsj@hotmail.com>
2021-03-25 10:13:23 -0400

    Remove clientId and Secret from NotifyAsync signature. This information can be retrieved from the connection info.

--- Source/ZoomNet/Resources/DataCompliance.cs
+++ Source/ZoomNet/Resources/DataCompliance.cs
@@ -1,5 +1,7 @@
 using Newtonsoft.Json.Linq;
 using Pathoschild.Http.Client;
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ZoomNet.Utilities;
@@ -12,6 +14,7 @@ namespace ZoomNet.Resources
 	/// </summary>
 	/// <seealso cref="ZoomNet.Resources.IDataCompliance" />
 	/// <remarks>
+	/// This resource can only be used when you connect to Zoom using OAuth. It cannot be used with a Jwt connection.
 	/// See <a href="https://marketplace.zoom.us/docs/api-reference/data-compliance/">Zoom documentation</a> for more information.
 	/// </remarks>
 	public class DataCompliance : IDataCompliance
@@ -31,8 +34,6 @@ namespace ZoomNet.Resources
 		/// Notify Zoom that you comply with the policy which requires you to handle user's
 		/// data in accordance to the user's preference after the user uninstalls your app.
 		/// </summary>
-		/// <param name="clientId">Your app's Client Id.</param>
-		/// <param name="secret">Your app's secret.</param>
 		/// <param name="userId">The Zoom user's id who uninstalled your app.</param>
 		/// <param name="accountId">The account Id.</param>
 		/// <param name="deauthorizationEventReceived">This object represents the payload of the webhook event sent by Zoom in your Deauthorization Endpoint Url after a user uninstalls your app. The values of the parameters in this object should be the same as that of the deauthorization event that you receive.</param>
@@ -40,8 +41,30 @@ namespace ZoomNet.Resources
 		/// <returns>
 		/// The async task.
 		/// </returns>
-		public Task NotifyAsync(string clientId, string secret, string userId, long accountId, object deauthorizationEventReceived, CancellationToken cancellationToken = default)
+		public Task NotifyAsync(string userId, long accountId, object deauthorizationEventReceived, CancellationToken cancellationToken = default)
 		{
+			// Prepare the request (but do not dispatch it yet)
+			var request = _client
+				.PostAsync("oauth/data/compliance")
+				.WithCancellationToken(cancellationToken);
+
+			// Figure out the client id and secret for authentication purposes
+			var tokenHandler = (ITokenHandler)request.Filters.Single(f => f.GetType().IsAssignableFrom(typeof(ITokenHandler)));
+			var secret = string.Empty;
+			var clientId = string.Empty;
+			switch (tokenHandler.ConnectionInfo)
+			{
+				case OAuthConnectionInfo oauthConnectionInfo:
+					secret = oauthConnectionInfo.ClientSecret;
+					clientId = oauthConnectionInfo.ClientId;
+					break;
+				case JwtConnectionInfo jwtConnectionInfo:
+					throw new Exception($"The DataCompliance resource cannot be use with a Jwt connection.");
+				default:
+					throw new Exception($"Unable to determine the connection secret and cient Id. {tokenHandler.ConnectionInfo.GetType()} is an unknown connection type.");
+			}
+
+			// Prepare the payload
 			var data = new JObject();
 			data.AddPropertyIfValue("client_id", clientId);
 			data.AddPropertyIfValue("user_id", userId);
@@ -49,16 +72,16 @@ namespace ZoomNet.Resources
 			data.AddPropertyIfValue("deauthorization_event_received", deauthorizationEventReceived);
 			data.AddPropertyIfValue("compliance_completed", "true");
 
-			var request = _client
-				.PostAsync("oauth/data/compliance")
-				.WithBasicAuthentication(clientId, secret)
-				.WithJsonBody(data)
-				.WithCancellationToken(cancellationToken);
-
 			// This endpoint relies on clientId+secret for authentication. It does not need tokens.
 			request.Filters.Remove<OAuthTokenHandler>();
 			request.Filters.Remove<JwtTokenHandler>();
 
+			// Authenticate using clientId+secret and also specify the payload
+			request = request
+				.WithBasicAuthentication(clientId, secret)
+				.WithJsonBody(data);
+
+			// Return the async task
 			return request
 				.AsMessage();
 		}
--- Source/ZoomNet/Resources/IDataCompliance.cs
+++ Source/ZoomNet/Resources/IDataCompliance.cs
@@ -8,6 +8,7 @@ namespace ZoomNet.Resources
 	/// user's data in accordance to the user's preference after the user uninstalls your app.
 	/// </summary>
 	/// <remarks>
+	/// This resource can only be used when you connect to Zoom using OAuth. It cannot be used with a Jwt connection.
 	/// See <a href="https://marketplace.zoom.us/docs/api-reference/data-compliance/">Zoom documentation</a> for more information.
 	/// </remarks>
 	public interface IDataCompliance
@@ -16,8 +17,6 @@ namespace ZoomNet.Resources
 		/// Notify Zoom that you comply with the policy which requires you to handle user's
 		/// data in accordance to the user's preference after the user uninstalls your app.
 		/// </summary>
-		/// <param name="clientId">Your app's Client Id.</param>
-		/// <param name="secret">Your app's secret.</param>
 		/// <param name="userId">The Zoom user's id who uninstalled your app.</param>
 		/// <param name="accountId">The account Id.</param>
 		/// <param name="deauthorizationEventReceived">This object represents the payload of the webhook event sent by Zoom in your Deauthorization Endpoint Url after a user uninstalls your app. The values of the parameters in this object should be the same as that of the deauthorization event that you receive.</param>
@@ -25,6 +24,6 @@ namespace ZoomNet.Resources
 		/// <returns>
 		/// The async task.
 		/// </returns>
-		Task NotifyAsync(string clientId, string secret, string userId, long accountId, object deauthorizationEventReceived, CancellationToken cancellationToken = default);
+		Task NotifyAsync(string userId, long accountId, object deauthorizationEventReceived, CancellationToken cancellationToken = default);
 	}
 }
