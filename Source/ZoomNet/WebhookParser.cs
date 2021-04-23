e40a5fa16635cc8bb309e0d1f8feb6ff4e22a8b8
Jericho <desautelsj@hotmail.com>
2021-04-23 11:52:37 -0400

    Add an interface for WebhookParser

--- Source/ZoomNet/WebhookParser.cs
+++ Source/ZoomNet/WebhookParser.cs
@@ -1,6 +1,4 @@
 using Newtonsoft.Json;
-using System.IO;
-using System.Threading.Tasks;
 using ZoomNet.Models.Webhooks;
 using ZoomNet.Utilities;
 
@@ -9,34 +7,12 @@ namespace ZoomNet
 	/// <summary>
 	/// Allows parsing of information posted from Zoom.
 	/// </summary>
-	public class WebhookParser
+	public class WebhookParser : IWebhookParser
 	{
-		#region PROPERTIES
-
 		/// <summary>
 		/// The name of the HTTP header where Zoom stores the verification token.
 		/// </summary>
 		public const string AUTHORIZATION_HEADER_NAME = "authorization";
-
-		#endregion
-
-		#region PUBLIC METHODS
-
-		/// <summary>
-		/// Parses the event webhook asynchronously.
-		/// </summary>
-		/// <param name="stream">The stream.</param>
-		/// <returns>An <see cref="Event" />.</returns>
-		public async Task<Event> ParseEventWebhookAsync(Stream stream)
-		{
-			string requestBody;
-			using (var streamReader = new StreamReader(stream))
-			{
-				requestBody = await streamReader.ReadToEndAsync().ConfigureAwait(false);
-			}
-
-			return ParseEventWebhook(requestBody);
-		}
 
 		/// <summary>
 		/// Parses the event webhook.
@@ -48,7 +24,5 @@ namespace ZoomNet
 			var webHookEvent = JsonConvert.DeserializeObject<Event>(requestBody, new WebHookEventConverter());
 			return webHookEvent;
 		}
-
-		#endregion
 	}
 }
