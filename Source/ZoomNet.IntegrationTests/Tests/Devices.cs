5c67e3f68db380868ca8cd8bebae47c272b2f0f5
Jericho <desautelsj@hotmail.com>
2021-05-15 15:57:28 -0400

    Remove commented out code

--- Source/ZoomNet.IntegrationTests/Tests/Devices.cs
+++ Source/ZoomNet.IntegrationTests/Tests/Devices.cs
@@ -27,28 +27,6 @@ namespace ZoomNet.IntegrationTests.Tests
 					await Task.Delay(250, cancellationToken).ConfigureAwait(false);    // Brief pause to ensure Zoom has time to catch up
 				});
 			await Task.WhenAll(cleanUpTasks).ConfigureAwait(false);
-
-			//// GET MY USER
-			//var myUser = await client.Users.GetCurrentAsync(cancellationToken).ConfigureAwait(false);
-			//await log.WriteLineAsync($"My user retrieved. My email address is {myUser.Email}").ConfigureAwait(false);
-
-			//// GET MY ASSISTANTS
-			//var myAssistants = await client.Users.GetAssistantsAsync(myUser.Id, cancellationToken).ConfigureAwait(false);
-			//await log.WriteLineAsync($"My user has {myAssistants.Length} assistants").ConfigureAwait(false);
-
-			//// GET MY SCHEDULERS
-			//var mySchedulers = await client.Users.GetSchedulersAsync(myUser.Id, cancellationToken).ConfigureAwait(false);
-			//await log.WriteLineAsync($"My user has {mySchedulers.Length} schedulers").ConfigureAwait(false);
-
-			//// GET MY SETTINGS
-			//var mySettings = await client.Users.GetSettingsAsync(myUser.Id, cancellationToken).ConfigureAwait(false);
-			//var myMeetingAuthSettings = await client.Users.GetMeetingAuthenticationSettingsAsync(myUser.Id, cancellationToken).ConfigureAwait(false);
-			//var myRecordingAuthSettings = await client.Users.GetRecordingAuthenticationSettingsAsync(myUser.Id, cancellationToken).ConfigureAwait(false);
-			//await log.WriteLineAsync("My settings retrieved").ConfigureAwait(false);
-
-			//// GET MY PERMISSIONS
-			//var myPermissions = await client.Users.GetPermissionsAsync(myUser.Id, cancellationToken).ConfigureAwait(false);
-			//await log.WriteLineAsync($"My permissions retrieved: I have been granted {myPermissions.Length} permissions").ConfigureAwait(false);
 		}
 	}
 }
