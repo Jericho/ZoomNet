58c976b4e3ba7b220ad450bc6e2bba17d6087040
jericho <desautelsj@hotmail.com>
2022-01-30 11:14:27 -0500

    Fix typos, remove unnecessary 'using' statements and do not invoke an obsolete method

--- Source/ZoomNet.IntegrationTests/Tests/Reports.cs
+++ Source/ZoomNet.IntegrationTests/Tests/Reports.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using ZoomNet.Models;
-using ZoomNet.Utilities;
 
 namespace ZoomNet.IntegrationTests.Tests
 {
@@ -20,18 +17,18 @@ namespace ZoomNet.IntegrationTests.Tests
 
 			//GET ALL MEETINGS
 			var totalMeetings = await client
-				.Meetings.GetAllAsync(userId, MeetingListType.Scheduled, 30, 1, cancellationToken)
+				.Meetings.GetAllAsync(userId, MeetingListType.Scheduled, 30, null, cancellationToken)
 				.ConfigureAwait(false);
 
 			var pastInstances = new List<PastInstance>();
 
-			foreach(var meeting in totalMeetings.Records)
+			foreach (var meeting in totalMeetings.Records)
 			{
 				var pastMeetingInstances = await client.PastMeetings.GetInstancesAsync(meeting.Id, cancellationToken);
 
-				foreach(var instance in pastMeetingInstances)
+				foreach (var instance in pastMeetingInstances)
 				{
-					if(instance.StartedOn < DateTime.UtcNow.AddDays(-1) && !instance.Uuid.StartsWith("/"))
+					if (instance.StartedOn < DateTime.UtcNow.AddDays(-1) && !instance.Uuid.StartsWith("/"))
 					{
 						pastInstances.Add(instance);
 					}
@@ -45,8 +42,8 @@ namespace ZoomNet.IntegrationTests.Tests
 				var paginatedParticipants = await client.Reports.GetMeetingParticipantsAsync(meeting.Uuid, 30, null, cancellationToken);
 				totalParticipants += paginatedParticipants.TotalRecords;
 			}
-			
-			await log.WriteLineAsync($"There are {pastInstances.Count} past isntances of meetings with a total of {totalParticipants} participants for this user.").ConfigureAwait(false);
+
+			await log.WriteLineAsync($"There are {pastInstances.Count} past instances of meetings with a total of {totalParticipants} participants for this user.").ConfigureAwait(false);
 		}
 	}
 }
