b8aacbb295cba8e776f1c4f3cc5e002859d15cc3
jericho <desautelsj@hotmail.com>
2022-02-19 14:30:17 -0500

    Fix polls
    Resolves #179

--- Source/ZoomNet/Models/PollQuestion.cs
+++ Source/ZoomNet/Models/PollQuestion.cs
@@ -26,12 +26,94 @@ namespace ZoomNet.Models
 		public PollQuestionType Type { get; set; }
 
 		/// <summary>
+		/// Gets or sets a value indicating whether the possible answers will be displayed as a drop-down box.
+		/// </summary>
+		[JsonProperty(PropertyName = "show_as_dropdown")]
+		public bool ShowAsDropdown { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the question must be answered.
+		/// </summary>
+		[JsonProperty(PropertyName = "answer_required")]
+		public bool IsRequired { get; set; }
+
+		/// <summary>
 		/// Gets or sets the answers to the question.
 		/// </summary>
 		/// <value>
 		/// The answers.
 		/// </value>
-		[JsonProperty("answer", NullValueHandling = NullValueHandling.Ignore)]
+		[JsonProperty("answers", NullValueHandling = NullValueHandling.Ignore)]
 		public string[] Answers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the correct answer(s) to the question.
+		/// </summary>
+		/// <value>
+		/// The answers.
+		/// </value>
+		[JsonProperty("right_answers", NullValueHandling = NullValueHandling.Ignore)]
+		public string[] CorrectAnswers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the information about the prompt questions.
+		/// This field only applies to questions of type 'Matching' and 'Rank'.
+		/// </summary>
+		/// <remarks>You must provide at least two prompts and no more than 10 prompts.</remarks>
+		[JsonProperty("prompts", NullValueHandling = NullValueHandling.Ignore)]
+		public PollPrompt[] Prompts { get; set; }
+
+		/// <summary>
+		/// Gets or sets the minimum number of characters.
+		/// This field only applies to questions of type 'Short' and 'Long'.
+		/// </summary>
+		/// <remarks>Must be greather or equal to 1.</remarks>
+		[JsonProperty(PropertyName = "answer_min_character")]
+		public int? MinimumNumberOfCharacters { get; set; }
+
+		/// <summary>
+		/// Gets or sets the maximum number of characters.
+		/// This field only applies to questions of type 'Short' and 'Long'.
+		/// </summary>
+		/// <remarks>Must be smaller or equal to 2,000.</remarks>
+		[JsonProperty(PropertyName = "answer_max_character")]
+		public int? MaximumNumberOfCharacters { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the corect answer is case sensitive.
+		/// This field only applies to questions of type 'Fill in the blanks'.
+		/// </summary>
+		[JsonProperty(PropertyName = "case_sensitive")]
+		public bool? IsCaseSensitive { get; set; }
+
+		/// <summary>
+		/// Gets or sets the rating minimum value.
+		/// This field only applies to questions of type 'Rating'.
+		/// </summary>
+		/// <remarks>Must be greather or equal to 0.</remarks>
+		[JsonProperty(PropertyName = "rating_min_value")]
+		public int? RatingMinimumValue { get; set; }
+
+		/// <summary>
+		/// Gets or sets the rating maximum value.
+		/// This field only applies to questions of type 'Rating'.
+		/// </summary>
+		/// <remarks>Must be smaller or equal to 10.</remarks>
+		[JsonProperty(PropertyName = "rating_max_value")]
+		public int? RatingMaximumValue { get; set; }
+
+		/// <summary>
+		/// Gets or sets the low score label.
+		/// This field only applies to questions of type 'Rating'.
+		/// </summary>
+		[JsonProperty(PropertyName = "rating_min_label")]
+		public string RatingLowScoreLabel { get; set; }
+
+		/// <summary>
+		/// Gets or sets the high score label.
+		/// This field only applies to questions of type 'Rating'.
+		/// </summary>
+		[JsonProperty(PropertyName = "rating_max_label")]
+		public string RatingHighScoreLabel { get; set; }
 	}
 }
