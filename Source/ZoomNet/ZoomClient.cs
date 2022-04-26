9746ed3ced62c924e73010b044cd3912a7e41a8c
Jericho <desautelsj@hotmail.com>
2022-04-26 09:42:39 -0400

    ﻿(GH-197) Add the Workspaces resource

--- Source/ZoomNet/ZoomClient.cs
+++ Source/ZoomNet/ZoomClient.cs
@@ -136,6 +136,9 @@ namespace ZoomNet
 		/// <inheritdoc/>
 		public IWebinars Webinars { get; private set; }
 
+		/// <inheritdoc/>
+		public IWorkspaces Workspaces { get; private set; }
+
 		#endregion
 
 		#region CTOR
@@ -250,6 +253,7 @@ namespace ZoomNet
 			TrackingFields = new TrackingFields(_fluentClient);
 			Users = new Users(_fluentClient);
 			Webinars = new Webinars(_fluentClient);
+			Workspaces = new Workspaces(_fluentClient);
 		}
 
 		/// <summary>
