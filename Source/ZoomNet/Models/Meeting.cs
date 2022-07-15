e1e04ec8af1da4a758c1c44b90fd730f9042ce89
Jericho <desautelsj@hotmail.com>
2022-07-15 10:54:47 -0400

    (GH-228) The meeting unique identifier in webhook JSON sometimes contains a string and sometimes an integer. This change allows the JSON parser to accept either data type.

--- Source/ZoomNet/Models/Meeting.cs
+++ Source/ZoomNet/Models/Meeting.cs
@@ -24,6 +24,12 @@ namespace ZoomNet.Models
 		/// The id.
 		/// </value>
 		[JsonPropertyName("id")]
+
+		// This allows us to overcome the fact that "id" is sometimes a string and sometimes a number
+		// See: https://devforum.zoom.us/t/the-data-type-of-meetingid-is-inconsistent-in-webhook-documentation/70090
+		// Also, see: https://github.com/Jericho/ZoomNet/issues/228
+		[JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+
 		public long Id { get; set; }
 
 		/// <summary>
