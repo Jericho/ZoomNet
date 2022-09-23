1838f3b39c3a1281c2dbfba5f8bdc80f9ada275b
Jericho <desautelsj@hotmail.com>
2022-09-23 12:12:54 -0400

    Add method to conveniently generate the response to a request from Zoom to validate your webhook URL

--- Source/ZoomNet/Models/Webhooks/EndpointUrlValidationEvent.cs
+++ Source/ZoomNet/Models/Webhooks/EndpointUrlValidationEvent.cs
@@ -15,5 +15,26 @@ namespace ZoomNet.Models.Webhooks
 		/// Gets or sets the token.
 		/// </summary>
 		public string PlainToken { get; set; }
+
+		/// <summary>
+		/// Generates the payload that should be returned to Zoom when your are asked to validate your webhook endpoint URL.
+		/// </summary>
+		/// <param name="secretToken">Your webhoop app's secret token.</param>
+		/// <returns>The payload to be returned to Zoom.</returns>
+		public string GenerateUrlValidationResponse(string secretToken)
+		{
+			// Generate the encrypted token according to Zoom's instructions: https://marketplace.zoom.us/docs/api-reference/webhook-reference/#verify-webhook-events
+			var crypto = new HMACSHA256(Encoding.UTF8.GetBytes(secretToken));
+			var encryptedToken = crypto.ComputeHash(Encoding.UTF8.GetBytes(this.PlainToken)).ToHexString();
+
+			var data = new JsonObject
+			{
+				{ "plainToken", this.PlainToken },
+				{ "encryptedToken", encryptedToken },
+			};
+
+			var jsonString = JsonSerializer.Serialize(data, typeof(JsonObject), ZoomNetJsonFormatter.SerializationContext);
+			return jsonString;
+		}
 	}
 }
