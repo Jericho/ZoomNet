07dce964b3fb7f6ddccd3b45e1dcf1219c5d0fb2
Jericho <desautelsj@hotmail.com>
2022-10-02 11:12:19 -0400

    (GH-16) Add the TrackingFields resource

--- Source/ZoomNet/ZoomClient.cs
+++ Source/ZoomNet/ZoomClient.cs
@@ -119,6 +119,9 @@ namespace ZoomNet
 		public ISms Sms { get; private set; }
 
 		/// <inheritdoc/>
+		public ITrackingFields TrackingFields { get; private set; }
+
+		/// <inheritdoc/>
 		public IUsers Users { get; private set; }
 
 		/// <inheritdoc/>
@@ -231,6 +234,7 @@ namespace ZoomNet
 			Reports = new Reports(_fluentClient);
 			Roles = new Roles(_fluentClient);
 			Sms = new Sms(_fluentClient);
+			TrackingFields = new TrackingFields(_fluentClient);
 			Users = new Users(_fluentClient);
 			Webinars = new Webinars(_fluentClient);
 		}
