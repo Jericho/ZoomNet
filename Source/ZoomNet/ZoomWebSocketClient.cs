c277407d837dc0eaa539213e7a330f4a1115d493
Jericho <desautelsj@hotmail.com>
2022-10-16 11:40:50 -0400

    (GH-242) Add a client for Websocket

---
+++ Source/ZoomNet/ZoomWebSocketClient.cs
@@ -0,0 +1,179 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Websocket.Client;
+using ZoomNet.Utilities;
+
+namespace ZoomNet
+{
+	public class ZoomWebSocketClient : IDisposable
+	{
+		private readonly string _clientId;
+		private readonly string _clientSecret;
+		private readonly string _accountId;
+		private readonly string _subscriptionId;
+		private readonly ILogger _logger;
+		private readonly IWebProxy _proxy;
+
+		private WebsocketClient _websocketClient;
+		private HttpClient _httpClient;
+		private OAuthConnectionInfo _connectionInfo;
+		private OAuthTokenHandler _tokenHandler;
+
+		public ZoomWebSocketClient(string clientId, string clientSecret, string accountId, string subscriptionId, IWebProxy proxy = null, ILogger logger = null)
+		{
+			_clientId = clientId;
+			_clientSecret = clientSecret;
+			_accountId = accountId;
+			_subscriptionId = subscriptionId;
+			_proxy = proxy;
+			_logger = logger ?? NullLogger.Instance;
+		}
+
+		public Task StartAsync()
+		{
+			_connectionInfo = new OAuthConnectionInfo(_clientId, _clientSecret, _accountId, null);
+			_httpClient = new HttpClient(new HttpClientHandler { Proxy = _proxy, UseProxy = _proxy != null });
+			_tokenHandler = new OAuthTokenHandler(_connectionInfo, _httpClient);
+
+			var clientFactory = new Func<Uri, CancellationToken, Task<WebSocket>>(async (uri, cancellationToken) =>
+			{
+				Console.WriteLine("Client factory invoked");
+
+				// The current value in the uri parameter must be ignored because it contains "access_token" which may or may not be expired.
+				// The following line ensures the "access_token" is refreshed whenever it expires.
+				uri = new Uri($"wss://ws.zoom.us/ws?subscriptionId={_subscriptionId}&access_token={_tokenHandler.Token}");
+
+				var client = new ClientWebSocket()
+				{
+					Options =
+					{
+						KeepAliveInterval = TimeSpan.Zero, // Turn off built-in "Keep Alive" feature because Zoom uses proprietary "heartbeat" every 30 seconds rather than standard "pong" messages at regular interval.
+						Proxy = _proxy,
+					}
+				};
+				client.Options.SetRequestHeader("ZoomNet-Version", ZoomClient.Version);
+
+				await client.ConnectAsync(uri, cancellationToken).ConfigureAwait(false);
+				return client;
+			});
+
+			_websocketClient = new WebsocketClient(new Uri("wss://ws.zoom.us"), clientFactory);
+			_websocketClient.Name = "ZoomNet";
+			_websocketClient.ReconnectTimeout = TimeSpan.FromSeconds(45); // Greater than 30 seconds because we send a heartbeat every 30 seconds
+			_websocketClient.ErrorReconnectTimeout = TimeSpan.FromSeconds(45);
+			_websocketClient.ReconnectionHappened.Subscribe(info => _logger.LogTrace($"Reconnection happened, type: {info.Type}"));
+			_websocketClient.DisconnectionHappened.Subscribe(info => _logger.LogTrace($"Disconnection happened, type: {info.Type}"));
+			_websocketClient.MessageReceived.Subscribe(ProcessMessage);
+
+			Task.Run(() => SendHeartbeat(_websocketClient));
+
+			return _websocketClient.Start();
+		}
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			// Call 'Dispose' to release resources
+			Dispose(true);
+
+			// Tell the GC that we have done the cleanup and there is nothing left for the Finalizer to do
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Releases unmanaged and - optionally - managed resources.
+		/// </summary>
+		/// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				ReleaseManagedResources();
+			}
+			else
+			{
+				// The object went out of scope and the Finalizer has been called.
+				// The GC will take care of releasing managed resources, therefore there is nothing to do here.
+			}
+
+			ReleaseUnmanagedResources();
+		}
+
+		private async Task SendHeartbeat(IWebsocketClient client)
+		{
+			while (true)
+			{
+				await Task.Delay(TimeSpan.FromSeconds(30)); // Zoom requires a heartbeat every 30 seconds
+
+				if (!client.IsRunning)
+				{
+					_logger.LogTrace("Client is not running. Skipping heartbeat");
+					continue;
+				}
+
+				_logger.LogTrace("Sending heartbeat");
+
+				await client.SendInstant("ping").ConfigureAwait(false);
+				client.Send("{\"module\":\"heartbeat\"}");
+			}
+		}
+
+		private void ProcessMessage(ResponseMessage msg)
+		{
+			var jsonDoc = JsonDocument.Parse(msg.Text);
+			var module = jsonDoc.RootElement.GetPropertyValue("module", string.Empty);
+			var content = jsonDoc.RootElement.GetPropertyValue("content", string.Empty);
+
+			switch (module)
+			{
+				case "build_connection":
+					_logger.LogTrace("Received message: {module}. Connection has been established.", module);
+					break;
+				case "heartbeat":
+					_logger.LogTrace("Received message: {module}. Server is acknowledging heartbeat.", module);
+					break;
+				case "message":
+					var parser = new WebhookParser();
+					var webhookEvent = parser.ParseEventWebhook(content);
+					var eventType = webhookEvent.EventType;
+					_logger.LogTrace("Received webhook event: {eventType}", eventType);
+					break;
+				default:
+					_logger.LogError("Received unknown message: {module}", module);
+					break;
+			}
+		}
+
+		private void ReleaseManagedResources()
+		{
+			_connectionInfo = null;
+			_tokenHandler = null;
+
+			if (_websocketClient != null)
+			{
+				_websocketClient.Dispose();
+				_websocketClient = null;
+			}
+
+			if (_httpClient != null)
+			{
+				_httpClient.Dispose();
+				_httpClient = null;
+			}
+		}
+
+		private void ReleaseUnmanagedResources()
+		{
+			// We do not hold references to unmanaged resources
+		}
+	}
+}
