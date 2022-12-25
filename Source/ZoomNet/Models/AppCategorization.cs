0b0c4abf888fdecc965a6beb3d8003e3fd103442
jericho <desautelsj@hotmail.com>
2022-12-25 13:02:37 -0500

    Add methods to get public and account created apps

---
+++ Source/ZoomNet/Models/AppCategorization.cs
@@ -0,0 +1,14 @@
+namespace ZoomNet.Models
+{
+	/// <summary>
+	/// Enumeration to indicate the usage categorization of an app.
+	/// </summary>
+	public enum AppCategorization
+	{
+		/// <summary>The app is under admin management.</summary>
+		UnderAdminManagement = 1,
+
+		/// <summary>Users have access to the app.</summary>
+		Normal = 2,
+	}
+}
