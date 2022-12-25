5fae93f05f3edf853a7c51071e7c1ee1ce44d07f
jericho <desautelsj@hotmail.com>
2022-12-25 13:02:37 -0500

    Add methods to get public and account created apps

--- Source/ZoomNet/Resources/Marketplace.cs
+++ Source/ZoomNet/Resources/Marketplace.cs
@@ -1,3 +1,9 @@
+using Pathoschild.Http.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ZoomNet.Models;
+
 namespace ZoomNet.Resources
 {
 	/// <inheritdoc/>
@@ -12,6 +18,34 @@ namespace ZoomNet.Resources
 		internal Marketplace(Pathoschild.Http.Client.IClient client)
 		{
 			_client = client;
+		}
+
+		/// <inheritdoc/>
+		public Task<PaginatedResponseWithToken<AppInfo>> GetPublicAppsAsync(int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			return GetAppsAsync<AppInfo>("public", recordsPerPage, pagingToken, cancellationToken);
+		}
+
+		/// <inheritdoc/>
+		public Task<PaginatedResponseWithToken<AppInfo>> GetCreatedAppsAsync(int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			return GetAppsAsync<AppInfo>("account_created", recordsPerPage, pagingToken, cancellationToken);
+		}
+
+		private Task<PaginatedResponseWithToken<T>> GetAppsAsync<T>(string type, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			if (recordsPerPage < 1 || recordsPerPage > 300)
+			{
+				throw new ArgumentOutOfRangeException(nameof(recordsPerPage), "Records per page must be between 1 and 300");
+			}
+
+			return _client
+				.GetAsync($"marketplace/apps")
+				.WithArgument("type", type)
+				.WithArgument("page_size", recordsPerPage)
+				.WithArgument("next_page_token", pagingToken)
+				.WithCancellationToken(cancellationToken)
+				.AsPaginatedResponseWithToken<T>("apps");
 		}
 	}
 }
