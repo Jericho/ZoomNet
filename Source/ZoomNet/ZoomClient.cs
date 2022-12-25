3e55465cb4b39f142c9f2c7046b125d0d532a4a4
jericho <desautelsj@hotmail.com>
2022-12-25 11:50:23 -0500

    Add the marketplace resource

--- Source/ZoomNet/ZoomClient.cs
+++ Source/ZoomNet/ZoomClient.cs
@@ -98,6 +98,9 @@ namespace ZoomNet
 		public IGroups Groups { get; private set; }
 
 		/// <inheritdoc/>
+		public IMarketplace Marketplace { get; private set; }
+
+		/// <inheritdoc/>
 		public IMeetings Meetings { get; private set; }
 
 		/// <inheritdoc/>
@@ -224,6 +227,7 @@ namespace ZoomNet
 			DataCompliance = new DataCompliance(_fluentClient);
 			ExternalContacts = new ExternalContacts(_fluentClient);
 			Groups = new Groups(_fluentClient);
+			Marketplace = new Marketplace(_fluentClient);
 			Meetings = new Meetings(_fluentClient);
 			PastMeetings = new PastMeetings(_fluentClient);
 			PastWebinars = new PastWebinars(_fluentClient);
