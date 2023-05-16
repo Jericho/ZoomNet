8131f0c422a3f019dea3adee3c2d139cb4892902
jericho <desautelsj@hotmail.com>
2023-05-16 12:18:04 -0400

    (GH-290) Add assistant_id to meeting model class

--- Source/ZoomNet/Models/Meeting.cs
+++ Source/ZoomNet/Models/Meeting.cs
@@ -135,5 +135,11 @@ namespace ZoomNet.Models
 		/// <value>The meeting timezone. For example, "America/Los_Angeles". Please reference our <a href="https://marketplace.zoom.us/docs/api-reference/other-references/abbreviation-lists#timezones">timezone list</a> for supported timezones and their formats.</value>
 		[JsonPropertyName("timezone")]
 		public string Timezone { get; set; }
+
+		/// <summary>
+		/// Gets or sets the ID of the user who scheduled this meeting on behalf of the host.
+		/// </summary>
+		[JsonPropertyName("assistant_id")]
+		public string AssistantId { get; set; }
 	}
 }
