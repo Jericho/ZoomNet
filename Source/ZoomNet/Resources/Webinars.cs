c81db02d140a9902ff4aa902a2494221646c3302
jericho <desautelsj@hotmail.com>
2024-04-03 10:32:10 -0400

    Fix parameters out of order

--- Source/ZoomNet/Resources/Webinars.cs
+++ Source/ZoomNet/Resources/Webinars.cs
@@ -953,7 +953,7 @@ namespace ZoomNet.Resources
 		/// <inheritdoc/>
 		public Task<InviteLink[]> CreateInviteLinksAsync(long webinarId, IEnumerable<string> names, long timeToLive = 7200, CancellationToken cancellationToken = default)
 		{
-			if (names == null || !names.Any()) throw new ArgumentNullException("You must provide at least one name", nameof(names));
+			if (names == null || !names.Any()) throw new ArgumentNullException(nameof(names), "You must provide at least one name");
 
 			var data = new JsonObject
 			{
