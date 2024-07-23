a89496b86e499648112c31187abf315dc4d51e1a
jericho <desautelsj@hotmail.com>
2024-07-22 21:21:01 -0400

    (GH-354) Add 'win 10' to the list of possible values that represent a Windows device

--- Source/ZoomNet/Models/ParticipantDevice.cs
+++ Source/ZoomNet/Models/ParticipantDevice.cs
@@ -29,7 +29,7 @@ namespace ZoomNet.Models
 		/// <summary>
 		/// The participant joined via VoIP using a Windows device.
 		/// </summary>
-		[MultipleValuesEnumMember(DefaultValue = "Windows", OtherValues = new[] { "WIN", "win 11" })]
+		[MultipleValuesEnumMember(DefaultValue = "Windows", OtherValues = new[] { "WIN", "win 10", "win 11" })]
 		Windows,
 
 		/// <summary>
