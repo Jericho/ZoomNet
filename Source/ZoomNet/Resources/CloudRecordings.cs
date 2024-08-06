e5ebd60b3b27e81098e07b9088d7ba30dc05473a
jericho <desautelsj@hotmail.com>
2024-08-05 20:46:18 -0400

    use inheritdoc instead of duplicating XML comment

--- Source/ZoomNet/Resources/CloudRecordings.cs
+++ Source/ZoomNet/Resources/CloudRecordings.cs
@@ -32,19 +32,7 @@ namespace ZoomNet.Resources
 			_client = client;
 		}
 
-		/// <summary>
-		/// Retrieve all cloud recordings for a user.
-		/// </summary>
-		/// <param name="userId">The user Id or email address.</param>
-		/// <param name="queryTrash">Indicates if you want to list recordings from trash.</param>
-		/// <param name="from">The start date.</param>
-		/// <param name="to">The end date.</param>
-		/// <param name="recordsPerPage">The number of records returned within a single API call.</param>
-		/// <param name="page">The current page number of returned records.</param>
-		/// <param name="cancellationToken">The cancellation token.</param>
-		/// <returns>
-		/// An array of <see cref="Recording">recordings</see>.
-		/// </returns>
+		/// <inheritdoc/>
 		[Obsolete("Zoom is in the process of deprecating the \"page number\" and \"page count\" fields.")]
 		public Task<PaginatedResponseWithTokenAndDateRange<Recording>> GetRecordingsForUserAsync(string userId, bool queryTrash = false, DateTime? from = null, DateTime? to = null, int recordsPerPage = 30, int page = 1, CancellationToken cancellationToken = default)
 		{
@@ -64,19 +52,7 @@ namespace ZoomNet.Resources
 				.AsPaginatedResponseWithTokenAndDateRange<Recording>("meetings");
 		}
 
-		/// <summary>
-		/// Retrieve all cloud recordings for a user.
-		/// </summary>
-		/// <param name="userId">The user Id or email address.</param>
-		/// <param name="queryTrash">Indicates if you want to list recordings from trash.</param>
-		/// <param name="from">The start date.</param>
-		/// <param name="to">The end date.</param>
-		/// <param name="recordsPerPage">The number of records returned within a single API call.</param>
-		/// <param name="pagingToken">The paging token.</param>
-		/// <param name="cancellationToken">The cancellation token.</param>
-		/// <returns>
-		/// An array of <see cref="Recording">recordings</see>.
-		/// </returns>
+		/// <inheritdoc/>
 		public Task<PaginatedResponseWithTokenAndDateRange<Recording>> GetRecordingsForUserAsync(string userId, bool queryTrash = false, DateTime? from = null, DateTime? to = null, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
 		{
 			if (recordsPerPage < 1 || recordsPerPage > 300)
