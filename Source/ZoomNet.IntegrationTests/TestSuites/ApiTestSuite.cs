e93012e21f0af5f1d04766ee62597143eaebe50f
jericho <desautelsj@hotmail.com>
2024-08-17 12:53:37 -0400

    Add Events resource

--- Source/ZoomNet.IntegrationTests/TestSuites/ApiTestSuite.cs
+++ Source/ZoomNet.IntegrationTests/TestSuites/ApiTestSuite.cs
@@ -16,6 +16,7 @@ namespace ZoomNet.IntegrationTests.TestSuites
 			typeof(CloudRecordings),
 			typeof(Contacts),
 			typeof(Dashboards),
+			typeof(Events),
 			typeof(ExternalContacts),
 			typeof(Groups),
 			typeof(Meetings),
