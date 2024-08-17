cea0c5ad77e14e1c126544516281f072280f88f1
jericho <desautelsj@hotmail.com>
2024-08-17 13:33:31 -0400

    Update serializer context

--- Source/ZoomNet/Json/ZoomNetJsonSerializerContext.cs
+++ Source/ZoomNet/Json/ZoomNetJsonSerializerContext.cs
@@ -11,6 +11,8 @@ namespace ZoomNet.Json
 
 	[JsonSerializable(typeof(ZoomNet.Models.Account))]
 	[JsonSerializable(typeof(ZoomNet.Models.AccountCallLog))]
+	[JsonSerializable(typeof(ZoomNet.Models.AppCategorization))]
+	[JsonSerializable(typeof(ZoomNet.Models.AppInfo))]
 	[JsonSerializable(typeof(ZoomNet.Models.ApprovalType))]
 	[JsonSerializable(typeof(ZoomNet.Models.AppStatus))]
 	[JsonSerializable(typeof(ZoomNet.Models.AppType))]
@@ -497,6 +499,8 @@ namespace ZoomNet.Json
 
 	[JsonSerializable(typeof(ZoomNet.Models.Account[]))]
 	[JsonSerializable(typeof(ZoomNet.Models.AccountCallLog[]))]
+	[JsonSerializable(typeof(ZoomNet.Models.AppCategorization[]))]
+	[JsonSerializable(typeof(ZoomNet.Models.AppInfo[]))]
 	[JsonSerializable(typeof(ZoomNet.Models.ApprovalType[]))]
 	[JsonSerializable(typeof(ZoomNet.Models.AppStatus[]))]
 	[JsonSerializable(typeof(ZoomNet.Models.AppType[]))]
