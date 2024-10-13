54a6f6402367ebd0ad92c779ce2abccc3ecbf87f
Emin Mešić <emin.mesic@outlook.com>
2024-10-13 18:38:05 +0200

    Add daily usage report. (#362)
    * Add daily usage report.
    
    * Rename DateObject to DailyUsageSummary.
    
    * Change type for date to be DateOnly.

---
+++ Source/ZoomNet/Models/DailyUsageSummary.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace ZoomNet.Models
+{
+	/// <summary>
+	/// Date Object.
+	/// </summary>
+	public class DailyUsageSummary
+	{
+		/// <summary>Gets or sets the date.</summary>
+		[JsonPropertyName("date")]
+		public DateOnly Date { get; set; }
+
+		/// <summary>Gets or sets number of meeting minutes.</summary>
+		[JsonPropertyName("meeting_minutes")]
+		public int MeetingMinutes { get; set; }
+
+		/// <summary>Gets or sets number of meetings.</summary>
+		[JsonPropertyName("meetings")]
+		public int Meetings { get; set; }
+
+		/// <summary>Gets or sets number of new users.</summary>
+		[JsonPropertyName("new_users")]
+		public int NewUsers { get; set; }
+
+		/// <summary>Gets or sets number of participants.</summary>
+		[JsonPropertyName("participants")]
+		public int Participants { get; set; }
+	}
+}
