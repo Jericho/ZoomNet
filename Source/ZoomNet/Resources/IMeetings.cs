83e5408ef31146f19dda7eb6169f3349d60070b0
Jericho <desautelsj@hotmail.com>
2024-10-28 11:51:56 -0400

    Add the generatePassword parameter when creting instant meetings

--- Source/ZoomNet/Resources/IMeetings.cs
+++ Source/ZoomNet/Resources/IMeetings.cs
@@ -54,6 +54,7 @@ namespace ZoomNet.Resources
 		/// <param name="topic">Meeting topic.</param>
 		/// <param name="agenda">Meeting description.</param>
 		/// <param name="password">Password to join the meeting. Password may only contain the following characters: [a-z A-Z 0-9 @ - _ *]. Max of 10 characters.</param>
+		/// <param name="generatePassword">Whether to generate a default passcode using the user's settings. If this value is true and the user has the PMI setting enabled with a passcode, then the user's meetings will use the PMI passcode.</param>
 		/// <param name="settings">Meeting settings.</param>
 		/// <param name="trackingFields">Tracking fields.</param>
 		/// <param name="templateId">Template Identifer.</param>
@@ -62,7 +63,7 @@ namespace ZoomNet.Resources
 		/// The new meeting.
 		/// </returns>
 		/// <exception cref="System.Exception">Thrown when an exception occured while creating the meeting.</exception>
-		Task<InstantMeeting> CreateInstantMeetingAsync(string userId, string topic, string agenda, string password = null, MeetingSettings settings = null, IDictionary<string, string> trackingFields = null, string templateId = null, CancellationToken cancellationToken = default);
+		Task<InstantMeeting> CreateInstantMeetingAsync(string userId, string topic, string agenda, string password = null, bool generatePassword = false, MeetingSettings settings = null, IDictionary<string, string> trackingFields = null, string templateId = null, CancellationToken cancellationToken = default);
 
 		/// <summary>
 		/// Create a scheduled meeting for a user.
@@ -74,7 +75,7 @@ namespace ZoomNet.Resources
 		/// <param name="duration">Meeting duration (minutes).</param>
 		/// <param name="timeZone">The time zone for start time.</param>
 		/// <param name="password">Password to join the meeting. Password may only contain the following characters: [a-z A-Z 0-9 @ - _ *]. Max of 10 characters.</param>
-		/// <param name="generatePassword">Whether to generate a default password using the user's settings.</param>
+		/// <param name="generatePassword">Whether to generate a default passcode using the user's settings. If this value is true and the user has the PMI setting enabled with a passcode, then the user's meetings will use the PMI passcode.</param>
 		/// <param name="settings">Meeting settings.</param>
 		/// <param name="trackingFields">Tracking fields.</param>
 		/// <param name="templateId">Template Identifer.</param>
@@ -96,7 +97,7 @@ namespace ZoomNet.Resources
 		/// <param name="recurrence">Recurrence information.</param>
 		/// <param name="timeZone">The time zone for start time.</param>
 		/// <param name="password">Password to join the meeting. Password may only contain the following characters: [a-z A-Z 0-9 @ - _ *]. Max of 10 characters.</param>
-		/// <param name="generatePassword">Whether to generate a default password using the user's settings.</param>
+		/// <param name="generatePassword">Whether to generate a default passcode using the user's settings. If this value is true and the user has the PMI setting enabled with a passcode, then the user's meetings will use the PMI passcode.</param>
 		/// <param name="settings">Meeting settings.</param>
 		/// <param name="trackingFields">Tracking fields.</param>
 		/// <param name="templateId">Template Identifer.</param>
