b6466cb226310fafa1435353941d992d5420f5f9
Jericho <desautelsj@hotmail.com>
2024-11-11 10:52:24 -0500

    Formatting

--- Source/ZoomNet/Resources/TrackingFields.cs
+++ Source/ZoomNet/Resources/TrackingFields.cs
@@ -5,21 +5,17 @@ using ZoomNet.Models;
 
 namespace ZoomNet.Resources
 {
-	/// <summary>
-	/// Allows you to manage tracking fields on your Zoom account.
-	/// </summary>
-	/// <remarks>
-	/// See <a href="https://marketplace.zoom.us/docs/api-reference/zoom-api/methods/#tag/Tracking-Field">Zoom documentation</a> for more information.
-	/// </remarks>
+	/// <inheritdoc/>
+
 	public class TrackingFields : ITrackingFields
 	{
-		private readonly Pathoschild.Http.Client.IClient _client;
+		private readonly IClient _client;
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="Accounts" /> class.
+		/// Initializes a new instance of the <see cref="TrackingFields" /> class.
 		/// </summary>
 		/// <param name="client">The HTTP client.</param>
-		internal TrackingFields(Pathoschild.Http.Client.IClient client)
+		internal TrackingFields(IClient client)
 		{
 			_client = client;
 		}
