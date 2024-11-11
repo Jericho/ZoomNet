6d5376f2afb0c31b90cded4791f5065ff75afa45
Jericho <desautelsj@hotmail.com>
2024-11-11 10:55:31 -0500

    Formatting

--- Source/ZoomNet/Resources/Marketplace.cs
+++ Source/ZoomNet/Resources/Marketplace.cs
@@ -10,13 +10,13 @@ namespace ZoomNet.Resources
 	/// <inheritdoc/>
 	public class Marketplace : IMarketplace
 	{
-		private readonly Pathoschild.Http.Client.IClient _client;
+		private readonly IClient _client;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Marketplace" /> class.
 		/// </summary>
 		/// <param name="client">The HTTP client.</param>
-		internal Marketplace(Pathoschild.Http.Client.IClient client)
+		internal Marketplace(IClient client)
 		{
 			_client = client;
 		}
