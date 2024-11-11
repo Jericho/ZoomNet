bb57a3a02215d1beb1de21dda0bc8c7352f76ba5
Jericho <desautelsj@hotmail.com>
2024-11-11 10:53:41 -0500

    Formatting

--- Source/ZoomNet/Resources/Rooms.cs
+++ Source/ZoomNet/Resources/Rooms.cs
@@ -6,41 +6,21 @@ using ZoomNet.Models;
 
 namespace ZoomNet.Resources
 {
-	/// <summary>
-	/// Allows you to manage rooms.
-	/// </summary>
-	/// <seealso cref="ZoomNet.Resources.IRooms" />
-	/// <remarks>
-	/// See <a href="https://marketplace.zoom.us/docs/api-reference/zoom-api/rooms-location/">Zoom documentation</a> for more information.
-	/// </remarks>
+	/// <inheritdoc/>
 	public class Rooms : IRooms
 	{
-		private readonly Pathoschild.Http.Client.IClient _client;
+		private readonly IClient _client;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Rooms" /> class.
 		/// </summary>
 		/// <param name="client">The HTTP client.</param>
-		internal Rooms(Pathoschild.Http.Client.IClient client)
+		internal Rooms(IClient client)
 		{
 			_client = client;
 		}
 
-		/// <summary>
-		/// Retrieve all rooms on your account.
-		/// </summary>
-		/// <param name="parentLocationId">
-		/// A unique identifier for the parent location.
-		/// For instance, if a Zoom Room is located in Floor 1 of Building A, the location of Building A will be the parent location of Floor 1.
-		/// Use this parameter to filter the response by a specific location hierarchy level.
-		/// </param>
-		/// <param name="type">Use this parameter to filter the response by the type of location.</param>
-		/// <param name="recordsPerPage">The number of records returned within a single API call.</param>
-		/// <param name="pagingToken">The paging token.</param>
-		/// <param name="cancellationToken">The cancellation token.</param>
-		/// <returns>
-		/// An array of <see cref="Room">rooms</see>.
-		/// </returns>
+		/// <inheritdoc/>
 		public Task<PaginatedResponseWithToken<Room>> GetAllAsync(string parentLocationId = null, RoomType? type = null, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
 		{
 			if (recordsPerPage < 1 || recordsPerPage > 300)
