edf815e4edfdca043a2037b7f998b18b8d8a0827
Arsslen Idadi <arsslen.idadi@gomycode365.onmicrosoft.com>
2025-02-22 18:51:11 -0500

    added computed arrays

--- Source/ZoomNet/Models/ChatMembersEditResult.cs
+++ Source/ZoomNet/Models/ChatMembersEditResult.cs
@@ -9,17 +9,38 @@ namespace ZoomNet.Models
 	public class ChatMembersEditResult
 	{
 		/// <summary>
+		/// Gets or sets the user IDs of the members added to the channel.
+		/// The user IDs of those who are not from the same account will be omitted from the list.
+		/// </summary>
+		[JsonIgnore]
+		public string[] Ids
+		{
+			get => CommaSeparatedIds?.Split(',') ?? [];
+			set => CommaSeparatedIds = string.Join(",", value ?? []);
+		}
+
+		/// <summary>
+		/// Gets or sets the member IDs of the members added to the channel.
+		/// </summary>
+		[JsonIgnore]
+		public string[] MemberIds
+		{
+			get => CommaSeparatedMemberIds?.Split(',') ?? [];
+			set => CommaSeparatedMemberIds = string.Join(",", value ?? []);
+		}
+
+		/// <summary>
 		/// Gets or sets the comma-separated user IDs of the members added to the channel.
 		/// The user IDs of those who are not from the same account will be omitted from the list.
 		/// </summary>
 		[JsonPropertyName("ids")]
-		public string Ids { get; set; }
+		public string CommaSeparatedIds { get; set; }
 
 		/// <summary>
 		/// Gets or sets the comma-separated member IDs of the members added to the channel.
 		/// </summary>
 		[JsonPropertyName("member_ids")]
-		public string MemberIds { get; set; }
+		public string CommaSeparatedMemberIds { get; set; }
 
 		/// <summary>
 		/// Gets or sets the date and time when the member(s) are added to the channel.
