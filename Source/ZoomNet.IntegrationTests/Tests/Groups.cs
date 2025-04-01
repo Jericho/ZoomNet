d85d23fd62bce1362c2cf5fbabfac808e0f1a7fa
Jericho <desautelsj@hotmail.com>
2025-03-31 20:32:32 -0400

    Add the following methods: GetAllAsync, GetAsync, CreateAsync, DeleteAsync, AddMembersByEmailAsync, AddMembersByIdAsync, AddAdministratorsByEmailAsync, AddAdministratorsByIdAsync, RemoveAdministratorAsync, RemoveMemberAsync, GetAdministratorsAsync, GetMembersAsync, UpdateAsync, UploadVirtualBackgroundAsync, DeleteVirtualBackgroundsAsync

--- Source/ZoomNet.IntegrationTests/Tests/Groups.cs
+++ Source/ZoomNet.IntegrationTests/Tests/Groups.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ZoomNet.Models;
@@ -17,6 +19,72 @@ namespace ZoomNet.IntegrationTests.Tests
 			var groups = await client.Groups.GetAllAsync(cancellationToken).ConfigureAwait(false);
 			await log.WriteLineAsync($"There are {groups.Length} groups").ConfigureAwait(false);
 
+			// CLEANUP PREVIOUS INTEGRATION TESTS THAT MIGHT HAVE BEEN INTERRUPTED BEFORE THEY HAD TIME TO CLEANUP AFTER THEMSELVES
+			var cleanUpTasks = groups
+				.Where(g => g.Name.StartsWith("ZoomNet Integration Testing:"))
+				.Select(async group =>
+				{
+					await client.Groups.DeleteAsync(group.Id, cancellationToken).ConfigureAwait(false);
+					await log.WriteLineAsync($"Group {group.Id} deleted").ConfigureAwait(false);
+					await Task.Delay(250, cancellationToken).ConfigureAwait(false);    // Brief pause to ensure Zoom has time to catch up
+				});
+			await Task.WhenAll(cleanUpTasks).ConfigureAwait(false);
+
+			// CREATE A NEW GROUP
+			var newGroup = await client.Groups.CreateAsync("ZoomNet Integration Testing: this is a test", cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"Group {newGroup.Id} created").ConfigureAwait(false);
+
+			await client.Groups.UpdateAsync(newGroup.Id, "ZoomNet Integration Testing: updated name", cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"The name of group {newGroup.Id} was updated").ConfigureAwait(false);
+
+			var group = await client.Groups.GetAsync(newGroup.Id, cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"Group {group.Id} retrieved").ConfigureAwait(false);
+
+			// UPLOAD BAKGROUND IMAGES
+			// Check that this computer has a folder containing sample images which we can use to send files to the channel
+			var samplePicturesFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "Samples");
+			if (Directory.Exists(samplePicturesFolder))
+			{
+				var rnd = new Random();
+				var samplePictures = Directory.EnumerateFiles(samplePicturesFolder, "*.jpg");
+				if (samplePictures.Any())
+				{
+					// UPLOAD A FILE
+					var samplePicture = samplePictures.ElementAt(rnd.Next(0, samplePictures.Count()));
+					using var fileToUploadStream = File.OpenRead(samplePicture);
+					var uploadedFileId = await client.Groups.UploadVirtualBackgroundAsync(group.Id, Path.GetFileName(samplePicture), fileToUploadStream, cancellationToken).ConfigureAwait(false);
+					await log.WriteLineAsync($"File {uploadedFileId} uploaded").ConfigureAwait(false);
+				}
+			}
+
+			// ADD AND REMOVE MEMBERS/ADMINISTRATORS
+			var memberId = await client.Groups.AddMemberByIdAsync(group.Id, myUser.Id, cancellationToken).ConfigureAwait(false);
+			if (!string.IsNullOrEmpty(memberId)) await log.WriteLineAsync($"{myUser.Id} added as a member to group {group.Id}").ConfigureAwait(false);
+
+			var adminId = await client.Groups.AddAdministratorByIdAsync(group.Id, myUser.Id, cancellationToken).ConfigureAwait(false);
+			if (!string.IsNullOrEmpty(adminId)) await log.WriteLineAsync($"{myUser.Id} added as an administrator to group {group.Id}").ConfigureAwait(false);
+
+			var members = await client.Groups.GetMembersAsync(group.Id, cancellationToken: cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"Group {group.Id} has {members.TotalRecords} members").ConfigureAwait(false);
+
+			var admins = await client.Groups.GetAdministratorsAsync(newGroup.Id, cancellationToken: cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"Group {group.Id} has {admins.TotalRecords} administrators").ConfigureAwait(false);
+
+			if (!string.IsNullOrEmpty(memberId))
+			{
+				await client.Groups.RemoveMemberAsync(newGroup.Id, memberId, cancellationToken).ConfigureAwait(false);
+				await log.WriteLineAsync($"{myUser.Id} removed as a member from group {newGroup.Id}").ConfigureAwait(false);
+			}
+
+			if (!string.IsNullOrEmpty(adminId))
+			{
+				await client.Groups.RemoveAdministratorAsync(newGroup.Id, adminId, cancellationToken).ConfigureAwait(false);
+				await log.WriteLineAsync($"{myUser.Id} removed as an administrator from group {newGroup.Id}").ConfigureAwait(false);
+			}
+
+			// DELETE THE GROUP
+			await client.Groups.DeleteAsync(group.Id, cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"Group {group.Id} deleted").ConfigureAwait(false);
 		}
 	}
 }
--- Source/ZoomNet/Resources/Groups.cs
+++ Source/ZoomNet/Resources/Groups.cs
@@ -1,10 +1,13 @@
 using Pathoschild.Http.Client;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
 using ZoomNet.Models;
+using ZoomNet.Utilities;
 
 namespace ZoomNet.Resources
 {
@@ -23,7 +26,48 @@ namespace ZoomNet.Resources
 		}
 
 		/// <inheritdoc/>
-		public async Task<string[]> AddUsersToGroupAsync(string groupId, IEnumerable<string> emails, CancellationToken cancellationToken = default)
+		public Task<Group[]> GetAllAsync(CancellationToken cancellationToken = default)
+		{
+			return _client
+				.GetAsync($"groups")
+				.WithCancellationToken(cancellationToken)
+				.AsObject<Group[]>("groups");
+		}
+
+		/// <inheritdoc/>
+		public Task<Group> GetAsync(string groupId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.GetAsync($"groups/{groupId}")
+				.WithCancellationToken(cancellationToken)
+				.AsObject<Group>();
+		}
+
+		/// <inheritdoc/>
+		public Task<Group> CreateAsync(string name, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "name", name }
+			};
+			return _client
+				.PostAsync($"groups")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsObject<Group>();
+		}
+
+		/// <inheritdoc/>
+		public Task DeleteAsync(string groupId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"groups/{groupId}")
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public async Task<string[]> AddMembersByEmailAsync(string groupId, IEnumerable<string> emails, CancellationToken cancellationToken = default)
 		{
 			var data = new JsonObject
 			{
@@ -41,12 +85,144 @@ namespace ZoomNet.Resources
 		}
 
 		/// <inheritdoc/>
-		public Task<Group[]> GetAllAsync(CancellationToken cancellationToken = default)
+		public async Task<string[]> AddMembersByIdAsync(string groupId, IEnumerable<string> userIds, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "members", userIds?.Select(e => new JsonObject { { "id", e } }).ToArray() }
+			};
+
+			var response = await _client
+				.PostAsync($"groups/{groupId}/members")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsJson()
+				.ConfigureAwait(false);
+
+			return response.GetPropertyValue("ids", string.Empty).Split(',');
+		}
+
+		/// <inheritdoc/>
+		public async Task<string[]> AddAdministratorsByEmailAsync(string groupId, IEnumerable<string> emails, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "members", emails?.Select(e => new JsonObject { { "email", e } }).ToArray() }
+			};
+			var response = await _client
+				.PostAsync($"groups/{groupId}/admins")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsJson()
+				.ConfigureAwait(false);
+
+			return response.GetPropertyValue("ids", string.Empty).Split(',');
+		}
+
+		/// <inheritdoc/>
+		public async Task<string[]> AddAdministratorsByIdAsync(string groupId, IEnumerable<string> userIds, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "members", userIds?.Select(e => new JsonObject { { "id", e } }).ToArray() }
+			};
+			var response = await _client
+				.PostAsync($"groups/{groupId}/admins")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsJson()
+				.ConfigureAwait(false);
+
+			return response.GetPropertyValue("ids", string.Empty).Split(',');
+		}
+
+		/// <inheritdoc/>
+		public Task RemoveAdministratorAsync(string groupId, string userId, CancellationToken cancellationToken = default)
 		{
 			return _client
-				.GetAsync($"groups")
+				.DeleteAsync($"groups/{groupId}/admins/{userId}")
 				.WithCancellationToken(cancellationToken)
-				.AsObject<Group[]>("groups");
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public Task RemoveMemberAsync(string groupId, string memberId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"groups/{groupId}/members/{memberId}")
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public Task<PaginatedResponseWithToken<GroupAdministrator>> GetAdministratorsAsync(string groupId, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			Utils.ValidateRecordPerPage(recordsPerPage);
+
+			return _client
+				.GetAsync($"groups/{groupId}/admins")
+				.WithArgument("page_size", recordsPerPage)
+				.WithArgument("next_page_token", pagingToken)
+				.WithCancellationToken(cancellationToken)
+				.AsPaginatedResponseWithToken<GroupAdministrator>("admins");
+		}
+
+		/// <inheritdoc/>
+		public Task<PaginatedResponseWithToken<GroupMember>> GetMembersAsync(string groupId, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			Utils.ValidateRecordPerPage(recordsPerPage);
+
+			return _client
+				.GetAsync($"groups/{groupId}/members")
+				.WithArgument("page_size", recordsPerPage)
+				.WithArgument("next_page_token", pagingToken)
+				.WithCancellationToken(cancellationToken)
+				.AsPaginatedResponseWithToken<GroupMember>("members");
+		}
+
+		/// <inheritdoc/>
+		public Task UpdateAsync(string groupId, string name, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "name", name }
+			};
+			return _client
+				.PatchAsync($"groups/{groupId}")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public Task<VirtualBackgroundFile> UploadVirtualBackgroundAsync(string groupId, string fileName, Stream pictureData, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.PostAsync($"groups/{groupId}/settings/virtual_backgrounds")
+				.WithBody(bodyBuilder =>
+				{
+					var content = new MultipartFormDataContent();
+
+					// Zoom requires the 'name' to be 'file'. Also, you
+					// must specify the 'fileName' otherwise Zoom will return
+					// a very confusing HTTP 400 error with the following body:
+					// {"code":-1,"message":"Required request part 'file' is not present"}
+					content.Add(new StreamContent(pictureData), "file", fileName);
+
+					return content;
+				})
+				.WithCancellationToken(cancellationToken)
+				.AsObject<VirtualBackgroundFile>();
+		}
+
+		/// <inheritdoc/>
+		public Task DeleteVirtualBackgroundsAsync(string groupId, string[] fileIds, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"groups/{groupId}/settings/virtual_backgrounds")
+				.WithArgument("file_ids", string.Join(",", fileIds))
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
 		}
 	}
 }
