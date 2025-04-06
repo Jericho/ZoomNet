7cd8547c96731b7346d9c614b075415184088031
Jericho <desautelsj@hotmail.com>
2025-04-05 21:25:51 -0400

    Fix xml comments

--- Source/ZoomNet/Models/TrackingField.cs
+++ Source/ZoomNet/Models/TrackingField.cs
@@ -10,38 +10,29 @@ namespace ZoomNet.Models
 		/// <summary>
 		/// Gets or sets the tracking field id.
 		/// </summary>
-		/// <value>
-		/// The id.
-		/// </value>
 		[JsonPropertyName("id")]
 		public string Id { get; set; }
 
 		/// <summary>
 		/// Gets or sets the name of the tracking field.
 		/// </summary>
-		/// <value>
-		/// The name of the tracking field.
-		/// </value>
 		[JsonPropertyName("field")]
 		public string Name { get; set; }
 
 		/// <summary>
 		/// Gets or sets the array of recommended values.
 		/// </summary>
-		/// <value>
-		/// The recommended values.
-		/// </value>
 		[JsonPropertyName("recommended_values")]
 		public string[] RecommendedValues { get; set; }
 
 		/// <summary>
-		/// Gets or sets a flag indicating whether the field is required.
+		/// Gets or sets a value indicating whether the field is required.
 		/// </summary>
 		[JsonPropertyName("required")]
 		public bool IsRequired { get; set; }
 
 		/// <summary>
-		/// Gets or sets a flag indicating whether the field is visible.
+		/// Gets or sets a value indicating whether the field is visible.
 		/// </summary>
 		[JsonPropertyName("visible")]
 		public bool IsVisible { get; set; }
