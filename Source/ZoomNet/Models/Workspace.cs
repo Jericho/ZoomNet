efb442ebfefc63599359b5273532b95219ed3ce3
Jericho <desautelsj@hotmail.com>
2025-04-29 20:01:47 -0400

    Create a new workspace

--- Source/ZoomNet/Models/Workspace.cs
+++ Source/ZoomNet/Models/Workspace.cs
@@ -9,14 +9,18 @@ namespace ZoomNet.Models
 	{
 		/// <summary>Gets or sets the workspace id.</summary>
 		[JsonPropertyName("id")]
-		public long Id { get; set; }
+		public string Id { get; set; }
 
 		/// <summary>Gets or sets the name of the workspace.</summary>
 		[JsonPropertyName("workspace_name")]
 		public string Name { get; set; }
 
+		/// <summary>Gets or sets the unique identifier of the location.</summary>
+		[JsonPropertyName("location_id")]
+		public string LocationId { get; set; }
+
 		/// <summary>Gets or sets the type of the workspace.</summary>
 		[JsonPropertyName("workspace_type")]
-		public string Type { get; set; }
+		public WorkspaceType Type { get; set; }
 	}
 }
