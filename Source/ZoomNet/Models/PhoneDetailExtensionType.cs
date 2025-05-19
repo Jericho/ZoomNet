e227bc0c82ac539212708efff6e2cebdb3f853ba
disccomp <disccomp@gmail.com>
2025-05-18 20:28:00 -0400

    Added ListPhonesAsync for phone/numbers

---
+++ Source/ZoomNet/Models/PhoneDetailExtensionType.cs
@@ -0,0 +1,46 @@
+using System.Runtime.Serialization;
+
+namespace ZoomNet.Models
+{
+	/// <summary>
+	/// Enumeration to indicate the extension type.
+	/// </summary>
+	public enum PhoneDetailExtensionType
+	{
+		/// <summary>
+		/// user.
+		/// </summary>
+		[EnumMember(Value = "user")]
+		User,
+		/// <summary>
+		/// callQueue.
+		/// </summary>
+		[EnumMember(Value = "callQueue")]
+		CallQueue,
+		/// <summary>
+		/// autoReceptionist.
+		/// </summary>
+		[EnumMember(Value = "autoReceptionist")]
+		AutoReceptionist,
+		/// <summary>
+		/// commonArea.
+		/// </summary>
+		[EnumMember(Value = "commonArea")]
+		CommonArea,
+		/// <summary>
+		/// emergencyNumberPool.
+		/// </summary>
+		[EnumMember(Value = "emergencyNumberPool")]
+		EmergencyNumberPool,
+		/// <summary>
+		/// companyLocation.
+		/// </summary>
+		[EnumMember(Value = "companyLocation")]
+		CompanyLocation,
+		/// <summary>
+		/// meetingService.
+		/// </summary>
+		[EnumMember(Value = "meetingService")]
+		MeetingService
+	}
+}
