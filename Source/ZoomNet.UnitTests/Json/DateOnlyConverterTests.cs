a49a08d742f1668b02319347d58322fbd7c03366
Jericho <desautelsj@hotmail.com>
2025-09-17 09:31:06 -0400

    Unit tests for DateOnlyConverterTests and DateTimeConverterTests.cs

---
+++ Source/ZoomNet.UnitTests/Json/DateOnlyConverterTests.cs
@@ -0,0 +1,170 @@
+using Shouldly;
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+using ZoomNet.Json;
+
+namespace ZoomNet.UnitTests.Json
+{
+	public class DateOnlyConverterTests
+	{
+		[Fact]
+		public void Read()
+		{
+			// Arrange
+			var json = "\"2025-09-17\"";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateOnlyConverter();
+
+			// Act
+			jsonReader.Read();
+			var result = converter.Read(ref jsonReader, objectType, options);
+
+			// Assert
+			result.ShouldBe((2025, 9, 17));
+		}
+
+		[Fact]
+		public void Throws_when_null_value()
+		{
+			// Arrange
+			var json = "{\"key\": null}";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateOnlyConverter();
+
+			// Act
+			jsonReader.Read(); // StartObject
+			jsonReader.Read(); // PropertyName (which in this example is "key")
+			jsonReader.Read(); // the Null value
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe("Unable to convert a null value to DateOnly");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Throws_when_empty_string()
+		{
+			// Arrange
+			var json = "\"\"";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateOnlyConverter();
+
+			// Act
+			jsonReader.Read();
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe("Unable to convert a null value to DateOnly");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Throws_when_too_many_parts()
+		{
+			// Arrange
+			var rawValue = "2025-09-17-09";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes($"\"{rawValue}\"");
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateOnlyConverter();
+
+			// Act
+			jsonReader.Read();
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe($"Unable to convert {rawValue} to DateOnly");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Throws_when_reading_any_other_data_type()
+		{
+			// Arrange
+			var json = "2025";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateOnlyConverter();
+
+			// Act
+			jsonReader.Read();
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe("Unable to convert Number to DateOnly");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Write()
+		{
+			// Arrange
+			var value = (2025, 9, 17);
+			var ms = new MemoryStream();
+			var jsonWriter = new Utf8JsonWriter(ms);
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateOnlyConverter();
+
+			// Act
+			converter.Write(jsonWriter, value, options);
+			jsonWriter.Flush();
+
+			ms.Position = 0;
+			var sr = new StreamReader(ms);
+			var result = sr.ReadToEnd();
+
+			// Assert
+			result.ShouldBe("\"2025-09-17\"");
+		}
+	}
+}
---
+++ Source/ZoomNet.UnitTests/Json/DateTimeConverterTests.cs
@@ -0,0 +1,142 @@
+using Shouldly;
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+using ZoomNet.Json;
+
+namespace ZoomNet.UnitTests.Json
+{
+	public class DateOnlyTimeonverterTests
+	{
+		[Fact]
+		public void Read()
+		{
+			// Arrange
+			var json = "\"2025-09-17T09:18:33\"";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateTimeConverter();
+
+			// Act
+			jsonReader.Read();
+			var result = converter.Read(ref jsonReader, objectType, options);
+
+			// Assert
+			result.ShouldBe(new DateTime(2025, 9, 17, 9, 18, 33));
+		}
+
+		[Fact]
+		public void Throws_when_null_value()
+		{
+			// Arrange
+			var json = "{\"key\": null}";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateTimeConverter();
+
+			// Act
+			jsonReader.Read(); // StartObject
+			jsonReader.Read(); // PropertyName (which in this example is "key")
+			jsonReader.Read(); // the Null value
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe("Unable to convert a null value to DateTime");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Throws_when_empty_string()
+		{
+			// Arrange
+			var json = "\"\"";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateTimeConverter();
+
+			// Act
+			jsonReader.Read();
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe("Unable to convert a null value to DateTime");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Throws_when_reading_any_other_data_type()
+		{
+			// Arrange
+			var json = "2025";
+			var jsonUtf8 = (ReadOnlySpan<byte>)Encoding.UTF8.GetBytes(json);
+			var jsonReader = new Utf8JsonReader(jsonUtf8);
+			var objectType = (Type)null;
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateTimeConverter();
+
+			// Act
+			jsonReader.Read();
+
+			try
+			{
+				var result = converter.Read(ref jsonReader, objectType, options);
+			}
+			catch (JsonException e)
+			{
+				e.Message.ShouldBe("Unable to convert Number to DateTime");
+
+				// Unfortunately, cannot use Should.Throw<JsonException>(() => converter.Read(ref jsonReader, objectType, options));
+				// because we can't use 'ref' arguments in lambda expressions.
+			}
+		}
+
+		[Fact]
+		public void Write()
+		{
+			// Arrange
+			var value = new DateTime(2025, 9, 17, 9, 20, 44, DateTimeKind.Utc);
+			var ms = new MemoryStream();
+			var jsonWriter = new Utf8JsonWriter(ms);
+			var options = new JsonSerializerOptions();
+
+			var converter = new DateTimeConverter();
+
+			// Act
+			converter.Write(jsonWriter, value, options);
+			jsonWriter.Flush();
+
+			ms.Position = 0;
+			var sr = new StreamReader(ms);
+			var result = sr.ReadToEnd();
+
+			// Assert
+			result.ShouldBe("\"2025-09-17T09:20:44Z\"");
+		}
+	}
+}
