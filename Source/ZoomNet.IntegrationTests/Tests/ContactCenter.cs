ef4134d899fa0f05947f8821174eb23f7a9eed50
Jericho <desautelsj@hotmail.com>
2025-09-20 09:13:09 -0400

    GetAllRolesAsync

--- Source/ZoomNet.IntegrationTests/Tests/ContactCenter.cs
+++ Source/ZoomNet.IntegrationTests/Tests/ContactCenter.cs
@@ -16,11 +16,8 @@ namespace ZoomNet.IntegrationTests.Tests
 			var paginatedUsers = await client.ContactCenter.SearchUsersAsync("*", null, null, 10, null, cancellationToken).ConfigureAwait(false);
 			await log.WriteLineAsync($"Found {paginatedUsers.TotalRecords} user profiles").ConfigureAwait(false);
 
-			var user = await client.ContactCenter.CreateUserAsync("zzz@example.com", cancellationToken).ConfigureAwait(false);
-			await log.WriteLineAsync($"User {user.Id} created").ConfigureAwait(false);
-
-			paginatedUsers = await client.ContactCenter.SearchUsersAsync("zzz", null, null, 10, null, cancellationToken).ConfigureAwait(false);
-			await log.WriteLineAsync($"Found {paginatedUsers.TotalRecords} user profiles").ConfigureAwait(false);
+			var paginatedRoles = await client.ContactCenter.GetAllRolesAsync(30, null, cancellationToken).ConfigureAwait(false);
+			await log.WriteLineAsync($"There are {paginatedRoles.TotalRecords} roles in Contact Center").ConfigureAwait(false);
 		}
 	}
 }
