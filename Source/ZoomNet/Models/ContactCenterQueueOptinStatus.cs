ad2a834f82e014cbf4e9c80d2c5327cb9105c10b
Jericho <desautelsj@hotmail.com>
2025-09-21 11:52:49 -0400

    Models I forgot to include in previous commit

---
+++ Source/ZoomNet/Models/ContactCenterQueueOptinStatus.cs
@@ -0,0 +1,17 @@
+namespace ZoomNet.Models
+{
+	/// <summary>
+	/// Enumeration to indicate the opt in / opt out status of an agent.
+	/// </summary>
+	public enum ContactCenterQueueOptinStatus
+	{
+		/// <summary>Opted in by agent himself or supervisor.</summary>
+		OptedIn = 0,
+
+		/// <summary>Opted out by supervisor or admin.</summary>
+		OptedOutBySupervisor = 1,
+
+		/// <summary>pted out by agent himself..</summary>
+		OptedOutByAgent = 2,
+	}
+}
