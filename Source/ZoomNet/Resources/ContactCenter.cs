807dbbc95cb8412abc1c81b4aa2cea369713e251
Jericho <desautelsj@hotmail.com>
2025-09-21 11:52:15 -0400

    ﻿GetAllQueuesAsync, DeleteQueueAsync, GetAllQueueAgentsAsync, UnassignAgentAsync, CreateQueueAsync, AssignAgentsAsync, DeleteRoleAsync, CreateRoleAsync, DeleteUserAsync

--- Source/ZoomNet/Resources/ContactCenter.cs
+++ Source/ZoomNet/Resources/ContactCenter.cs
@@ -23,6 +23,151 @@ namespace ZoomNet.Resources
 			_client = client;
 		}
 
+
+		#region Agent Statuses
+
+		#endregion
+
+		#region Asset Library
+
+		#endregion
+
+		#region Call Control
+
+		#endregion
+
+		#region Campaigns
+
+		#endregion
+
+		#region Dispositions
+
+		#endregion
+
+		#region Engagements
+
+		#endregion
+
+		#region Flows
+
+		#endregion
+
+		#region Inboxes
+
+		#endregion
+
+		#region Logs
+
+		#endregion
+
+		#region Notes
+
+		#endregion
+
+		#region Operating Hours
+
+		#endregion
+
+		#region Queues
+
+		/// <inheritdoc/>
+		public Task<PaginatedResponseWithToken<ContactCenterQueue>> GetAllQueuesAsync(ContactCenterQueueChannel? channel = null, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			Utils.ValidateRecordPerPage(recordsPerPage);
+
+			return _client
+				.GetAsync("contact_center/queues")
+				.WithArgument("channel", channel?.ToEnumString())
+				.WithArgument("page_size", recordsPerPage)
+				.WithArgument("next_page_token", pagingToken)
+				.WithCancellationToken(cancellationToken)
+				.AsPaginatedResponseWithToken<ContactCenterQueue>("queues");
+		}
+
+		/// <inheritdoc/>
+		public Task DeleteQueueAsync(string queueId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"contact_center/queues/{queueId}")
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public Task<PaginatedResponseWithToken<ContactCenterQueueAgent>> GetAllQueueAgentsAsync(string queueId, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
+		{
+			Utils.ValidateRecordPerPage(recordsPerPage);
+
+			return _client
+				.GetAsync($"contact_center/queues/{queueId}/agents")
+				.WithArgument("page_size", recordsPerPage)
+				.WithArgument("next_page_token", pagingToken)
+				.WithCancellationToken(cancellationToken)
+				.AsPaginatedResponseWithToken<ContactCenterQueueAgent>("agents");
+		}
+
+		/// <inheritdoc/>
+		public Task UnassignAgentAsync(string queueId, string userId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"contact_center/queues/{queueId}/agents/{userId}")
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public Task<ContactCenterQueue> CreateQueueAsync(string name, string description = null, ContactCenterQueueChannel channel = ContactCenterQueueChannel.Video, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "queue_name", name },
+				{ "queue_description", description },
+				{ "channel", channel.ToEnumString() },
+				{ "channel_types", new[] { channel.ToEnumString() } } // Documentation says "channel_types" is deprecated but the API throws an error if it's not used
+			};
+
+			return _client
+				.PostAsync("contact_center/queues")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsObject<ContactCenterQueue>();
+		}
+
+		/// <inheritdoc/>
+		public Task AssignAgentsAsync(string queueId, IEnumerable<string> userIds, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "user_ids", userIds?.ToArray() }
+			};
+
+			return _client
+				.PostAsync($"contact_center/queues/{queueId}/agents")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		#endregion
+
+		#region Recordings
+
+		#endregion
+
+		#region Regions
+
+		#endregion
+
+		#region Reports v2 (CX analytics)
+
+		#endregion
+
+		#region Reports (Legacy Reports)
+
+		#endregion
+
+		#region Roles
+
 		/// <inheritdoc/>
 		public Task<PaginatedResponseWithToken<ContactCenterUserRole>> GetAllRolesAsync(int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
 		{
@@ -35,6 +180,48 @@ namespace ZoomNet.Resources
 				.WithCancellationToken(cancellationToken)
 				.AsPaginatedResponseWithToken<ContactCenterUserRole>("roles");
 		}
+
+		/// <inheritdoc/>
+		public Task DeleteRoleAsync(string roleId, string transferRoleId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"contact_center/roles/{roleId}")
+				.WithArgument("transfer_role_id", transferRoleId)
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <inheritdoc/>
+		public Task<ContactCenterUserRole> CreateRoleAsync(string name, string descirption, IEnumerable<string> privileges, CancellationToken cancellationToken = default)
+		{
+			var data = new JsonObject
+			{
+				{ "role_name", name },
+				{ "role_description", descirption },
+				{ "privileges", privileges?.ToArray() }
+			};
+			return _client
+				.PostAsync("contact_center/roles")
+				.WithJsonBody(data)
+				.WithCancellationToken(cancellationToken)
+				.AsObject<ContactCenterUserRole>();
+		}
+
+		#endregion
+
+		#region Routing Profiles
+
+		#endregion
+
+		#region Skills
+
+		#endregion
+
+		#region SMS
+
+		#endregion
+
+		#region Users
 
 		/// <inheritdoc/>
 		public Task<PaginatedResponseWithToken<ContactCenterUser>> SearchUsersAsync(string keyword, string regionId = null, UserStatus? status = null, int recordsPerPage = 30, string pagingToken = null, CancellationToken cancellationToken = default)
@@ -120,5 +307,20 @@ namespace ZoomNet.Resources
 				.WithCancellationToken(cancellationToken)
 				.AsObject<ContactCenterUser>();
 		}
+
+		/// <inheritdoc/>
+		public Task DeleteUserAsync(string userId, CancellationToken cancellationToken = default)
+		{
+			return _client
+				.DeleteAsync($"contact_center/users/{userId}")
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		#endregion
+
+		#region Variables
+
+		#endregion
 	}
 }
