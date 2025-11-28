c31a7cf6cfa64b0ac40fff9250823de34ed147f7
Jericho <desautelsj@hotmail.com>
2025-11-28 13:15:40 -0500

    Debugging code to investigate issue:https://devforum.zoom.us/t/getting-meeting-transcript-info-works-with-uuid-but-not-with-meeting-id/138985
    I intend to revert this commit prior to merging this branch with the develop branch.

--- Source/ZoomNet.IntegrationTests/Tests/CloudRecordings.cs
+++ Source/ZoomNet.IntegrationTests/Tests/CloudRecordings.cs
@@ -10,6 +10,22 @@ namespace ZoomNet.IntegrationTests.Tests
 	{
 		public async Task RunAsync(User myUser, string[] myPermissions, IZoomClient client, TextWriter log, CancellationToken cancellationToken)
 		{
+			//===========================================================================
+			// This is for debuggin purposes, while investigating this issue: https://devforum.zoom.us/t/getting-meeting-transcript-info-works-with-uuid-but-not-with-meeting-id/138985
+
+			// Fetching transcript info using meeting UUID works as expected
+			var bbb = await client.CloudRecordings.GetTranscriptInfoAsync("j3xXxYijTjigTGf4rPci%2Fw%3D%3D", cancellationToken).ConfigureAwait(false);
+			if (bbb.DownloadRestrictionReason == TranscriptRestrictionReason.Available) await log.WriteLineAsync($"The meeting transcript info is available here: {bbb.DownloadUrl}").ConfigureAwait(false);
+			else await log.WriteLineAsync($"The meeting transcript info is not available because: {bbb.DownloadRestrictionReason.ToEnumString()}").ConfigureAwait(false);
+
+			// According to documentation, using the metingId should work too, but it does not
+			bbb = await client.CloudRecordings.GetTranscriptInfoAsync(83584776469, cancellationToken).ConfigureAwait(false);
+			if (bbb.DownloadRestrictionReason == TranscriptRestrictionReason.Available) await log.WriteLineAsync($"The meeting transcript info is available here: {bbb.DownloadUrl}").ConfigureAwait(false);
+			else await log.WriteLineAsync($"The meeting transcript info is not available because: {bbb.DownloadRestrictionReason.ToEnumString()}").ConfigureAwait(false);
+			//===========================================================================
+
+
+
 			await log.WriteLineAsync("\n***** CLOUD RECORDINGS *****\n").ConfigureAwait(false);
 
 			// GET ALL THE RECORDINGS FOR A GIVEN USER
