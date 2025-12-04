d194d31db001b73a169204eca9dba32f6d0d5e98
Jericho <desautelsj@hotmail.com>
2025-12-04 09:39:10 -0500

    Mark CallLog as obsolete

--- Source/ZoomNet/Resources/ICallLogs.cs
+++ Source/ZoomNet/Resources/ICallLogs.cs
@@ -11,6 +11,7 @@ namespace ZoomNet.Resources
 	/// <remarks>
 	/// See <a href="https://marketplace.zoom.us/docs/api-reference/phone/methods/#tag/Call-Logs">Zoom documentation</a> for more information.
 	/// </remarks>
+	[Obsolete("Zoom will sunset the CallLogs API endpoints on fully deprecated and sunset on May 30, 2026. Please use the CallHistory instead. For more information, please refer to https://developers.zoom.us/docs/phone/migrate/")]
 	public interface ICallLogs
 	{
 		/// <summary>
