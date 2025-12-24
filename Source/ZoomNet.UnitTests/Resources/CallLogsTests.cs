c7918c77f5c97f7ab8d336c7848c7951e8be991d
Jericho <desautelsj@hotmail.com>
2025-12-24 13:58:10 -0500

    Additional unit tests for the CallLogs resource

--- Source/ZoomNet.UnitTests/Resources/CallLogsTests.cs
+++ Source/ZoomNet.UnitTests/Resources/CallLogsTests.cs
@@ -316,5 +316,597 @@ namespace ZoomNet.UnitTests.Resources
 			result.Records.ShouldNotBeNull();
 			result.Records.Length.ShouldBe(5);
 		}
+
+		#region User Call Logs Tests
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithMinimalParameters()
+		{
+			// Arrange
+			var userId = "user123";
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""call_logs"": [
+					{
+						""id"": ""log1"",
+						""caller_number"": ""1234"",
+						""callee_number"": ""5678"",
+						""direction"": ""inbound"",
+						""duration"": 60,
+						""date_time"": ""2023-12-06T10:00:00Z""
+					}
+				]
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("type", "all")
+				.WithQueryString("page_size", "30")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+			result.Records.ShouldNotBeEmpty();
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithDateRange()
+		{
+			// Arrange
+			var userId = "user123";
+			var from = new DateTime(2023, 12, 1);
+			var to = new DateTime(2023, 12, 31);
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("from", "2023-12-01")
+				.WithQueryString("to", "2023-12-31")
+				.WithQueryString("type", "all")
+				.WithQueryString("page_size", "30")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, from, to, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithCallType()
+		{
+			// Arrange
+			var userId = "user123";
+			var type = CallLogType.Missed;
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("type", "missed")
+				.WithQueryString("page_size", "30")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, type: type, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithPhoneNumber()
+		{
+			// Arrange
+			var userId = "user123";
+			var phoneNumber = "+1234567890";
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("type", "all")
+				.WithQueryString("phone_number", phoneNumber)
+				.WithQueryString("page_size", "30")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, phoneNumber: phoneNumber, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithPagination()
+		{
+			// Arrange
+			var userId = "user123";
+			var recordsPerPage = 50;
+			var pagingToken = "token123";
+			var userCallLogsJson = @"{
+				""next_page_token"": ""token456"",
+				""page_size"": 50,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("type", "all")
+				.WithQueryString("page_size", "50")
+				.WithQueryString("next_page_token", "token123")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, recordsPerPage: recordsPerPage, pagingToken: pagingToken, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+			result.RecordsPerPage.ShouldBe(50);
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithAllParameters()
+		{
+			// Arrange
+			var userId = "user123";
+			var from = new DateTime(2023, 11, 1);
+			var to = new DateTime(2023, 11, 30);
+			var type = CallLogType.Missed;
+			var phoneNumber = "+9876543210";
+			var recordsPerPage = 100;
+			var pagingToken = "full_token";
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 100,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("from", "2023-11-01")
+				.WithQueryString("to", "2023-11-30")
+				.WithQueryString("type", "missed")
+				.WithQueryString("phone_number", phoneNumber)
+				.WithQueryString("page_size", "100")
+				.WithQueryString("next_page_token", "full_token")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, from, to, type, phoneNumber, recordsPerPage, pagingToken, TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_WithDifferentCallTypes()
+		{
+			// Arrange
+			var userId = "user123";
+			var type = CallLogType.All;
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("type", "all")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, type: type, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetUserCallLogsAsync_MissedType()
+		{
+			// Arrange
+			var userId = "user123";
+			var type = CallLogType.Missed;
+			var userCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_logs"))
+				.WithQueryString("type", "missed")
+				.Respond("application/json", userCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(userId, type: type, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		#endregion
+
+		#region Account Call Logs Tests
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithMinimalParameters()
+		{
+			// Arrange
+			var accountCallLogsJson = @"{
+				""next_page_token"": """",
+				""page_size"": 30,
+				""from"": ""2025-11-01"",
+				""to"": ""2025-12-01"",
+				""call_logs"": []
+			}";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("type", "all")
+				.WithQueryString("timeType", "startTime")
+				.WithQueryString("charged_call_logs", false.ToString())
+				.WithQueryString("page_size", "30")
+				.Respond("application/json", accountCallLogsJson);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithPathType()
+		{
+			// Arrange
+			var pathType = CallLogPathType.Pstn;
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("path", "pstn")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(pathType: pathType, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithTimeType()
+		{
+			// Arrange
+			var timeType = CallLogTimeType.EndTime;
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("timeType", "endTime")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(timeType: timeType, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithSiteId()
+		{
+			// Arrange
+			var siteId = "site123";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("site_id", siteId)
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(siteId: siteId, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithChargedCallLogs()
+		{
+			// Arrange
+			var chargedCallLogs = true;
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("charged_call_logs", true.ToString())
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(chargedCallLogs: chargedCallLogs, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithAllParameters()
+		{
+			// Arrange
+			var from = new DateTime(2023, 10, 1);
+			var to = new DateTime(2023, 10, 31);
+			var type = CallLogType.Missed;
+			var pathType = CallLogPathType.Extension;
+			var timeType = CallLogTimeType.EndTime;
+			var siteId = "site456";
+			var chargedCallLogs = true;
+			var recordsPerPage = 75;
+			var pagingToken = "complete_token";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("from", "2023-10-01")
+				.WithQueryString("to", "2023-10-31")
+				.WithQueryString("type", "missed")
+				.WithQueryString("path", "extension")
+				.WithQueryString("timeType", "endTime")
+				.WithQueryString("site_id", "site456")
+				.WithQueryString("charged_call_logs", true.ToString())
+				.WithQueryString("page_size", "75")
+				.WithQueryString("next_page_token", "complete_token")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(from, to, type, pathType, timeType, siteId, chargedCallLogs, recordsPerPage, pagingToken, TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithDifferentPathTypes()
+		{
+			// Arrange
+			var pathType = CallLogPathType.AutoReceptionist;
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("path", "autoReceptionist")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(pathType: pathType, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithCallQueuePathType()
+		{
+			// Arrange
+			var pathType = CallLogPathType.CallQueue;
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("path", "callQueue")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(pathType: pathType, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithCustomPageSize()
+		{
+			// Arrange
+			var recordsPerPage = 200;
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("page_size", "200")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(recordsPerPage: recordsPerPage, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithPagingToken()
+		{
+			// Arrange
+			var pagingToken = "next_page_xyz";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("next_page_token", pagingToken)
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(pagingToken: pagingToken, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithOnlyFromDate()
+		{
+			// Arrange
+			var from = new DateTime(2023, 9, 1);
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("from", "2023-09-01")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(from: from, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		[Fact]
+		public async Task GetAccountCallLogsAsync_WithOnlyToDate()
+		{
+			// Arrange
+			var to = new DateTime(2023, 9, 30);
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs"))
+				.WithQueryString("to", "2023-09-30")
+				.Respond("application/json", MULTIPLE_ACCOUNT_CALL_LOGS_JSON);
+
+			var logger = _outputHelper.ToLogger<IZoomClient>();
+			var client = Utils.GetFluentClient(mockHttp, logger: logger);
+			var calllogs = new CallLogs(client);
+
+			// Act
+			var result = await calllogs.GetAsync(to: to, cancellationToken: TestContext.Current.CancellationToken);
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+		}
+
+		#endregion
 	}
 }
