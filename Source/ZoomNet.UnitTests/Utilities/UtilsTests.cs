ae68253ddfe5398be3760e0f2329210848a466ce
Jericho <desautelsj@hotmail.com>
2026-01-13 19:37:55 -0500

    Unit tests for Utils.EncodeUUID

--- Source/ZoomNet.UnitTests/Utilities/UtilsTests.cs
+++ Source/ZoomNet.UnitTests/Utilities/UtilsTests.cs
@@ -612,5 +612,484 @@ namespace ZoomNet.UnitTests.Utilities
 		}
 
 		#endregion
+
+		#region EncodeUUID Tests - Null and Empty Values
+
+		[Fact]
+		public void EncodeUUID_WithNull_ReturnsNull()
+		{
+			// Arrange
+			string uuid = null;
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBeNull();
+		}
+
+		[Fact]
+		public void EncodeUUID_WithEmptyString_ReturnsEmptyString()
+		{
+			// Arrange
+			var uuid = string.Empty;
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(string.Empty);
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Values Without Forward Slash
+
+		[Fact]
+		public void EncodeUUID_WithoutForwardSlash_ReturnsUnchanged()
+		{
+			// Arrange
+			var uuid = "abc123-def456";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(uuid);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithStandardUUID_ReturnsUnchanged()
+		{
+			// Arrange
+			var uuid = "550e8400-e29b-41d4-a716-446655440000";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(uuid);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithAlphanumeric_ReturnsUnchanged()
+		{
+			// Arrange
+			var uuid = "Meeting123";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(uuid);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithSpecialCharactersButNoSlash_ReturnsUnchanged()
+		{
+			// Arrange
+			var uuid = "uuid-with-dashes_and_underscores";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(uuid);
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Values With Forward Slash
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlash_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "abc/123";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldNotBe(uuid);
+			result.ShouldBe("abc%252f123");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAtStart_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "/abc123";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe("%252fabc123");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAtEnd_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "abc123/";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe("abc123%252f");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithMultipleForwardSlashes_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "abc/def/ghi";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe("abc%252fdef%252fghi");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithConsecutiveForwardSlashes_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "abc//def";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe("abc%252f%252fdef");
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Mixed Content
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAndOtherSpecialChars_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "abc/def-ghi_jkl";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+			result.ShouldBe("abc%252fdef-ghi_jkl");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAndSpaces_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "abc 123/def 456";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+			result.ShouldContain("%2b");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAndQueryParams_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "meetingId/123?param=value";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Real-World Scenarios
+
+		[Fact]
+		public void EncodeUUID_WithZoomMeetingId_NoSlash_ReturnsUnchanged()
+		{
+			// Typical Zoom meeting ID without slash
+			// Arrange
+			var uuid = "85641287426";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(uuid);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithZoomMeetingUUID_WithSlash_ReturnsDoubleEncoded()
+		{
+			// Some Zoom UUIDs contain forward slashes
+			// Arrange
+			var uuid = "M2IzNTY5/6DTP8aVIZ5ASVQ==";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+			result.ShouldNotContain("/");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithBase64LikeUUID_WithSlash_ReturnsDoubleEncoded()
+		{
+			// Base64-encoded values might contain forward slashes
+			// Arrange
+			var uuid = "dGVzdC9kYXRh/encoded==";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Boundary Cases
+
+		[Fact]
+		public void EncodeUUID_WithOnlyForwardSlash_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "/";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe("%252f");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithVeryLongStringWithSlash_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = new string('a', 100) + "/" + new string('b', 100);
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+			result.Length.ShouldBeGreaterThan(uuid.Length);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithWhitespace_NoSlash_ReturnsUnchanged()
+		{
+			// Arrange
+			var uuid = "   ";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldBe(uuid);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithWhitespaceAndSlash_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = " / ";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Encoding Verification
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlash_ResultIsDoubleEncoded()
+		{
+			// Verify that double encoding produces %252F (encoded %2F)
+			// Arrange
+			var uuid = "test/value";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			// Single encoding would produce %2F
+			// Double encoding produces %252F (the % itself is encoded as %25)
+			result.ShouldBe("test%252fvalue");
+			result.ShouldNotContain("%2F");
+			result.ShouldNotContain("/");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlash_VerifiesDoubleEncoding()
+		{
+			// Verify that the encoding produces the correct double-encoded format
+			// Arrange
+			var uuid = "meeting/123";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			// Double encoding: / -> %2F -> %252F
+			result.ShouldBe("meeting%252f123");
+			result.ShouldContain("%252f");
+			result.ShouldNotContain("/");
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Multiple Calls
+
+		[Fact]
+		public void EncodeUUID_CalledMultipleTimes_WithSameValue_ReturnsConsistentResult()
+		{
+			// Arrange
+			var uuid = "test/uuid";
+
+			// Act
+			var result1 = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+			var result2 = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+			var result3 = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result1.ShouldBe(result2);
+			result2.ShouldBe(result3);
+		}
+
+		[Fact]
+		public void EncodeUUID_WithDifferentValues_ReturnsDistinctResults()
+		{
+			// Arrange
+			var uuid1 = "test/uuid1";
+			var uuid2 = "test/uuid2";
+
+			// Act
+			var result1 = ZoomNet.Utilities.Utils.EncodeUUID(uuid1);
+			var result2 = ZoomNet.Utilities.Utils.EncodeUUID(uuid2);
+
+			// Assert
+			result1.ShouldNotBe(result2);
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Edge Cases with Other URL-Unsafe Characters
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAndAmpersand_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "test/value&param";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+			result.ShouldContain("%2526");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAndPercent_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "test/100%";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+			result.ShouldContain("%2525");
+		}
+
+		[Fact]
+		public void EncodeUUID_WithForwardSlashAndPlus_ReturnsDoubleEncoded()
+		{
+			// Arrange
+			var uuid = "test/uuid+value";
+
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+
+			// Assert
+			result.ShouldContain("%252f");
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Theory-Based Tests
+
+		[Theory]
+		[InlineData(null, null)]
+		[InlineData("", "")]
+		[InlineData("simple", "simple")]
+		[InlineData("uuid-123", "uuid-123")]
+		[InlineData("UUID_456", "UUID_456")]
+		public void EncodeUUID_WithoutSlash_ReturnsExpectedValue(string input, string expected)
+		{
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(input);
+
+			// Assert
+			result.ShouldBe(expected);
+		}
+
+		[Theory]
+		[InlineData("a/b", "a%252fb")]
+		[InlineData("/test", "%252ftest")]
+		[InlineData("test/", "test%252f")]
+		[InlineData("a/b/c", "a%252fb%252fc")]
+		[InlineData("//", "%252f%252f")]
+		public void EncodeUUID_WithSlash_ReturnsDoubleEncodedValue(string input, string expected)
+		{
+			// Act
+			var result = ZoomNet.Utilities.Utils.EncodeUUID(input);
+
+			// Assert
+			result.ShouldBe(expected);
+		}
+
+		#endregion
+
+		#region EncodeUUID Tests - Performance
+
+		[Fact]
+		public void EncodeUUID_RepeatedCalls_PerformsConsistently()
+		{
+			// Arrange
+			var uuid = "test/uuid";
+			var iterations = 10000;
+
+			// Act & Assert
+			Should.NotThrow(() =>
+			{
+				for (int i = 0; i < iterations; i++)
+				{
+					var result = ZoomNet.Utilities.Utils.EncodeUUID(uuid);
+				}
+			});
+		}
+
+		#endregion
 	}
 }
