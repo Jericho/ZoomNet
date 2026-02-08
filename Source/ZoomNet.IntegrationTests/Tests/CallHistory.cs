88631e767f09e1b7b61047a8c9e59f49a2da5a9f
Jericho <desautelsj@hotmail.com>
2026-02-07 19:29:07 -0500

    Fix CallHistory tests

--- Source/ZoomNet.IntegrationTests/Tests/CallHistory.cs
+++ Source/ZoomNet.IntegrationTests/Tests/CallHistory.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ZoomNet.Models;
@@ -13,13 +12,14 @@ namespace ZoomNet.IntegrationTests.Tests
 		{
 			await log.WriteLineAsync("\n***** CALL HISTORY *****\n").ConfigureAwait(false);
 
-			var from = DateTime.UtcNow.Subtract(TimeSpan.FromDays(30));
-			var to = DateTime.UtcNow;
+			var now = DateTime.UtcNow;
+			var from = DateOnly.FromDateTime(now.Subtract(TimeSpan.FromDays(30)));
+			var to = DateOnly.FromDateTime(now);
 
 			var accountCallHistory = await client.CallHistory.GetAccountCallHistoryAsync(from, to, cancellationToken: cancellationToken);
 			await log.WriteLineAsync($"There are {accountCallHistory.TotalRecords} items in the account call history").ConfigureAwait(false);
 
-			if (accountCallHistory.Records.Any())
+			if (accountCallHistory.Records.Length > 0)
 			{
 				var firstCallElement = await client.CallHistory.GetCallElementAsync(accountCallHistory.Records[0].Id, cancellationToken: cancellationToken);
 				await log.WriteLineAsync($"The first call element was betwee {firstCallElement.CallerName} and {firstCallElement.CalleeName}").ConfigureAwait(false);
--- Source/ZoomNet.UnitTests/Resources/CallHistoryTests.cs
+++ Source/ZoomNet.UnitTests/Resources/CallHistoryTests.cs
@@ -4,7 +4,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
-using ZoomNet.Resources;
+using ZoomNet.Models;
 using ZoomNet.UnitTests.Properties;
 
 namespace ZoomNet.UnitTests.Resources
@@ -22,8 +22,8 @@ namespace ZoomNet.UnitTests.Resources
 		public async Task GetAccountCallHistoryAsync()
 		{
 			// Arrange
-			var from = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-			var to = new DateTime(2024, 1, 31, 23, 59, 59, DateTimeKind.Utc);
+			var from = new DateOnly(2024, 1, 1);
+			var to = new DateOnly(2024, 1, 31);
 
 			var mockHttp = new MockHttpMessageHandler();
 			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_history"))
@@ -34,7 +34,7 @@ namespace ZoomNet.UnitTests.Resources
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetAccountCallHistoryAsync(from, to, cancellationToken: TestContext.Current.CancellationToken);
@@ -51,27 +51,27 @@ namespace ZoomNet.UnitTests.Resources
 		public async Task GetAccountCallHistoryAsync_WithCallType()
 		{
 			// Arrange
-			var from = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-			var to = new DateTime(2024, 1, 31, 23, 59, 59, DateTimeKind.Utc);
-			var callType = "inbound";
+			var from = new DateOnly(2024, 1, 1);
+			var to = new DateOnly(2024, 1, 31);
+			var callType = CallElementCallType.General;
 
 			var mockHttp = new MockHttpMessageHandler();
 			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_history"))
 				.WithQueryString("from", from.ToString("yyyy-MM-dd"))
 				.WithQueryString("to", to.ToString("yyyy-MM-dd"))
-				.WithQueryString("type", callType)
+				.WithQueryString("type", callType.ToEnumString())
 				.WithQueryString("page_size", "30")
 				.Respond("application/json", EndpointsResource.phone_call_history_GET);
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetAccountCallHistoryAsync(
 				from,
 				to,
-				type: callType,
+				callTypes: [callType],
 				cancellationToken: TestContext.Current.CancellationToken);
 
 			// Assert
@@ -84,60 +84,24 @@ namespace ZoomNet.UnitTests.Resources
 		public async Task GetAccountCallHistoryAsync_WithPathFilter()
 		{
 			// Arrange
-			var from = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-			var to = new DateTime(2024, 1, 31, 23, 59, 59, DateTimeKind.Utc);
-			var path = "inbound";
+			var from = new DateOnly(2024, 1, 1);
+			var to = new DateOnly(2024, 1, 31);
 
 			var mockHttp = new MockHttpMessageHandler();
 			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_history"))
 				.WithQueryString("from", from.ToString("yyyy-MM-dd"))
 				.WithQueryString("to", to.ToString("yyyy-MM-dd"))
-				.WithQueryString("path", path)
 				.WithQueryString("page_size", "30")
 				.Respond("application/json", EndpointsResource.phone_call_history_GET);
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetAccountCallHistoryAsync(
 				from,
 				to,
-				path: path,
-				cancellationToken: TestContext.Current.CancellationToken);
-
-			// Assert
-			mockHttp.VerifyNoOutstandingExpectation();
-			mockHttp.VerifyNoOutstandingRequest();
-			result.ShouldNotBeNull();
-		}
-
-		[Fact]
-		public async Task GetAccountCallHistoryAsync_WithTimeType()
-		{
-			// Arrange
-			var from = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-			var to = new DateTime(2024, 1, 31, 23, 59, 59, DateTimeKind.Utc);
-			var timeType = "startTime";
-
-			var mockHttp = new MockHttpMessageHandler();
-			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_history"))
-				.WithQueryString("from", from.ToString("yyyy-MM-dd"))
-				.WithQueryString("to", to.ToString("yyyy-MM-dd"))
-				.WithQueryString("time_type", timeType)
-				.WithQueryString("page_size", "30")
-				.Respond("application/json", EndpointsResource.phone_call_history_GET);
-
-			var logger = _outputHelper.ToLogger<IZoomClient>();
-			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
-
-			// Act
-			var result = await callHistory.GetAccountCallHistoryAsync(
-				from,
-				to,
-				timeType: timeType,
 				cancellationToken: TestContext.Current.CancellationToken);
 
 			// Assert
@@ -150,11 +114,10 @@ namespace ZoomNet.UnitTests.Resources
 		public async Task GetAccountCallHistoryAsync_WithAllFilters()
 		{
 			// Arrange
-			var from = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-			var to = new DateTime(2024, 1, 31, 23, 59, 59, DateTimeKind.Utc);
-			var callType = "inbound";
-			var path = "inbound";
-			var timeType = "startTime";
+			var from = new DateOnly(2024, 1, 1);
+			var to = new DateOnly(2024, 1, 31);
+			var callType = CallElementCallType.Emergency;
+			var timeType = CallLogTimeType.StartTime;
 			var recordsPerPage = 100;
 			var pagingToken = "token_xyz";
 
@@ -162,23 +125,21 @@ namespace ZoomNet.UnitTests.Resources
 			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_history"))
 				.WithQueryString("from", from.ToString("yyyy-MM-dd"))
 				.WithQueryString("to", to.ToString("yyyy-MM-dd"))
-				.WithQueryString("type", callType)
-				.WithQueryString("path", path)
-				.WithQueryString("time_type", timeType)
+				.WithQueryString("type", callType.ToEnumString())
+				.WithQueryString("time_type", timeType.ToEnumString())
 				.WithQueryString("page_size", "100")
 				.WithQueryString("next_page_token", pagingToken)
 				.Respond("application/json", EndpointsResource.phone_call_history_GET);
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetAccountCallHistoryAsync(
 				from,
 				to,
-				type: callType,
-				path: path,
+				callTypes: [callType],
 				timeType: timeType,
 				recordsPerPage: recordsPerPage,
 				pagingToken: pagingToken,
@@ -199,11 +160,11 @@ namespace ZoomNet.UnitTests.Resources
 
 			var mockHttp = new MockHttpMessageHandler();
 			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_history", callId))
-				.Respond("application/json", EndpointsResource.phone_call_history__callId__GET);
+				.Respond("application/json", EndpointsResource.phone_call_history__callHistoryUuid__GET);
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetCallElementAsync(callId, TestContext.Current.CancellationToken);
@@ -234,7 +195,7 @@ namespace ZoomNet.UnitTests.Resources
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetUserCallHistoryAsync(
@@ -258,27 +219,27 @@ namespace ZoomNet.UnitTests.Resources
 			var userId = "user123";
 			var from = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 			var to = new DateTime(2024, 1, 31, 23, 59, 59, DateTimeKind.Utc);
-			var callType = "outbound";
+			var callType = CallElementCallType.General;
 			var recordsPerPage = 50;
 
 			var mockHttp = new MockHttpMessageHandler();
 			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "users", userId, "call_history"))
 				.WithQueryString("from", from.ToString("yyyy-MM-dd"))
 				.WithQueryString("to", to.ToString("yyyy-MM-dd"))
-				.WithQueryString("type", callType)
+				.WithQueryString("type", callType.ToEnumString())
 				.WithQueryString("page_size", "50")
 				.Respond("application/json", EndpointsResource.phone_users__userId__call_history_GET);
 
 			var logger = _outputHelper.ToLogger<IZoomClient>();
 			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
+			var callHistory = new ZoomNet.Resources.CallHistory(client);
 
 			// Act
 			var result = await callHistory.GetUserCallHistoryAsync(
 				userId,
 				from,
 				to,
-				type: callType,
+				callTypes: [callType],
 				recordsPerPage: recordsPerPage,
 				cancellationToken: TestContext.Current.CancellationToken);
 
@@ -287,30 +248,6 @@ namespace ZoomNet.UnitTests.Resources
 			mockHttp.VerifyNoOutstandingRequest();
 			result.ShouldNotBeNull();
 			result.RecordsPerPage.ShouldBe(50);
-		}
-
-		[Fact]
-		public async Task GetCallLogAsync()
-		{
-			// Arrange
-			var callLogId = "log123456";
-
-			var mockHttp = new MockHttpMessageHandler();
-			mockHttp.Expect(HttpMethod.Get, Utils.GetZoomApiUri("phone", "call_logs", callLogId))
-				.Respond("application/json", EndpointsResource.phone_call_logs__callLogId__GET);
-
-			var logger = _outputHelper.ToLogger<IZoomClient>();
-			var client = Utils.GetFluentClient(mockHttp, logger: logger);
-			var callHistory = new CallHistory(client);
-
-			// Act
-			var result = await callHistory.GetCallLogAsync(callLogId, TestContext.Current.CancellationToken);
-
-			// Assert
-			mockHttp.VerifyNoOutstandingExpectation();
-			mockHttp.VerifyNoOutstandingRequest();
-			result.ShouldNotBeNull();
-			result.Id.ShouldBe(callLogId);
 		}
 	}
 }
