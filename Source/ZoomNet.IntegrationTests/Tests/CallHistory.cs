4efa36159b76181f0e35043b795b0baf9dca844d
Jericho <desautelsj@hotmail.com>
2026-02-10 21:08:16 -0500

    Fix how we serialize arrays on the querystring
    Based on my discussions with Gianni and Elisa

--- Source/ZoomNet.IntegrationTests/Tests/CallHistory.cs
+++ Source/ZoomNet.IntegrationTests/Tests/CallHistory.cs
@@ -15,14 +15,15 @@ namespace ZoomNet.IntegrationTests.Tests
 			var now = DateTime.UtcNow;
 			var from = DateOnly.FromDateTime(now.Subtract(TimeSpan.FromDays(30)));
 			var to = DateOnly.FromDateTime(now);
+			var directions = new[] { CallElementDirection.Inbound, CallElementDirection.Outbound };
 
-			var accountCallHistory = await client.CallHistory.GetAccountCallHistoryAsync(from, to, cancellationToken: cancellationToken);
+			var accountCallHistory = await client.CallHistory.GetAccountCallHistoryAsync(from, to, directions: directions, cancellationToken: cancellationToken);
 			await log.WriteLineAsync($"There are {accountCallHistory.TotalRecords} items in the account call history").ConfigureAwait(false);
 
 			if (accountCallHistory.Records.Length > 0)
 			{
-				var firstCallElement = await client.CallHistory.GetCallElementAsync(accountCallHistory.Records[0].Id, cancellationToken: cancellationToken);
-				await log.WriteLineAsync($"The first call element was betwee {firstCallElement.CallerName} and {firstCallElement.CalleeName}").ConfigureAwait(false);
+				var firstCallElement = await client.CallHistory.GetCallElementAsync(accountCallHistory.Records[0].CallHistoryUuid, cancellationToken: cancellationToken);
+				await log.WriteLineAsync($"The first call element was between {firstCallElement.CallerName} and {firstCallElement.CalleeName}").ConfigureAwait(false);
 			}
 		}
 	}
